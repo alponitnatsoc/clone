44525ad5be4ec308133c72f698c00acfb9c2941e
nicomalacho <nicolasgaviria94@gmail.com>
2015-10-23 22:08:47 -0500

    se organiza el controlador

--- src/RocketSeller/TwoPickBundle/Controller/ProcedureController.php
+++ src/RocketSeller/TwoPickBundle/Controller/ProcedureController.php
@@ -35,9 +35,67 @@ class ProcedureController extends Controller
      *                                 		->sort_order
      * @return integer $priority       prioridad del empleador (vip, regular)
      */
-    public function validateAction()//$id_employer, $id_procedure_type, $priority, $id_user, $employees)
-    {		
-    		//datos de prueba    		
+    public function validateAction($id_employer, $id_procedure_type, $priority, $id_user, $employees, $id_action_type)
+    {		  		
+    		$employerSearch = $this->loadClassById($id_employer,"Employer");
+    		$userSearch = $this->loadClassById($id_user,"User");
+    		$actionTypeSearch = $this->loadClassById($id_action_type,"ActionType");
+    		$em = $this->getDoctrine()->getManager();										
+    		foreach ($employees as $employee) {
+    			foreach ($employee["entities"] as $entity) {
+    				$actionTypeFound = $this->loadClassById($entity["id_action_type"],"ActionType");	    				
+		    		$employeeFound = $this->loadClassById($employee["id_employee"],"Employee");
+    				$employeeHasEntityFound = $this->loadClassByArray(array("employeeEmployee" => $employeeFound),"EmployeeHasEntity");    			  		  
+				    	if($employeeHasEntityFound && $employeeHasEntityFound->getEntityEntity()->getEntityTypeEntityType()->getIdEntityType() == $entity["id_entity"]){
+				    		echo "el empleado con id:". $employee["id_employee"]." esta afiliado a la entidad: ".$employeeHasEntityFound->getEntityEntity()->getEntityTypeEntityType()->getName().  "<br></br>";
+				    	}else{				    		
+		    				$action = new Action();	    		            
+				            $action->setUserUser($userSearch);
+				            $action->setActionTypeActionType($actionTypeSearch);
+				            $action->setPersonPerson($employeeFound->getPersonPerson());
+				            $em->persist($action);
+				            $em->flush();
+				            echo "el empleado con el id: " .$employee["id_employee"]. " no esta afiliado a la entidad con id: ". $entity["id_entity"]. "<br></br>";
+				    	}
+	    				$action = new Action();
+			            $action->setUserUser($userSearch);
+			            $action->setActionTypeActionType($actionTypeFound);
+			            $action->setPersonPerson($employeeFound->getPersonPerson());
+			            $em->persist($action);
+			            $em->flush();				 
+    			}
+    		}		    
+    }
+    /**
+     * hace un query de la clase para instanciarla
+     * @param  [type] $parameter id que desea pasar
+     * @param  [type] $entity    entidad a la cual hace referencia
+     */
+    public function loadClassById($parameter, $entity)
+    {
+		$loadedClass = $this->getdoctrine()
+		->getRepository('RocketSellerTwoPickBundle:'.$entity)
+		->find($parameter);
+		return $loadedClass;		
+    }
+    /**
+     * hace un query de la clase para instanciarla
+     * @param  [type] $array  array de parametros que desea pasar
+     * @param  [type] $entity entidad a la cual hace referencia
+     */
+    public function loadClassByArray($array, $entity)
+    {
+		$loadedClass = $this->getdoctrine()
+		->getRepository('RocketSellerTwoPickBundle:'.$entity)
+		->findOneBy($array);
+		return $loadedClass;		
+    }
+    /**
+     * metodo que llama el metodo validate con las variables inicializadas 
+     * tambien se describe la estructura que debe de tener el array de employees
+     */
+    public function testValidateAction()
+    {
     		$id_employer =1;
     		$id_procedure_type = "Inscripcion";
     		$priority = 1;
@@ -82,50 +140,8 @@ class ProcedureController extends Controller
 			    				)
 		    				)
     			);
+    		$this->validateAction($id_employer, $id_procedure_type, $priority, $id_user, $employees, $id_action_type);
 
-	    		$employerSearch = $this->getDoctrine()
-	    		->getRepository('RocketSellerTwoPickBundle:Employer')
-	    		->find($id_employer);
-	    		$userSearch = $this->getdoctrine()
-	    		->getRepository('RocketSellerTwoPickBundle:User')
-	    		->find($id_user);
-	    		$actionTypeSearch = $this->getdoctrine()
-	    		->getRepository('RocketSellerTwoPickBundle:ActionType')
-	    		->find($id_action_type);
-	    		$em = $this->getDoctrine()->getManager();										
-    		foreach ($employees as $employee) {
-    			foreach ($employee["entities"] as $entity) {
-    				$actionTypeFound = $this->getdoctrine()
-		    		->getRepository('RocketSellerTwoPickBundle:ActionType')
-		    		->find($entity["id_action_type"]);		    				
-		    		$employeeFound = $this->getDoctrine()
-		    		->getRepository('RocketSellerTwoPickBundle:Employee')
-		    		->find($employee["id_employee"]);
-    				$employeeHasEntityFound = $this->getDoctrine()
-			    		->getRepository('RocketSellerTwoPickBundle:EmployeeHasEntity')
-			    		->findOneBy(
-			    			array(
-		    			 		"employeeEmployee" => $employeeFound
-		    			   	)
-	    			   	);		    		  		    	
-				    	if($employeeHasEntityFound && $employeeHasEntityFound->getEntityEntity()->getEntityTypeEntityType()->getIdEntityType() == $entity["id_entity"]){
-				    		echo "el empleado con id:". $employee["id_employee"]." esta afiliado a la entidad: ".$employeeHasEntityFound->getEntityEntity()->getEntityTypeEntityType()->getName().  "<br></br>";
-				    	}else{				    		
-		    				$action = new Action();	    		            
-				            $action->setUserUser($userSearch);
-				            $action->setActionTypeActionType($actionTypeSearch);
-				            $action->setPersonPerson($employeeFound->getPersonPerson());
-				            $em->persist($action);
-				            $em->flush();
-				            echo "el empleado con el id: " .$employee["id_employee"]. " no esta afiliado a la entidad con id: ". $entity["id_entity"]. "<br></br>";
-				    	}
-	    				$action = new Action();
-			            $action->setUserUser($userSearch);
-			            $action->setActionTypeActionType($actionTypeFound);
-			            $action->setPersonPerson($employeeFound->getPersonPerson());
-			            $em->persist($action);
-			            $em->flush();				 
-    			}
-    		}		    
     }
+
 }
