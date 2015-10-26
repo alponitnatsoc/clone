163e19b9cfdb5cf661f9b7f8340604ad736cabfb
nicomalacho <nicolasgaviria94@gmail.com>
2015-10-26 17:37:17 -0500

    actualizacion del validador de tramites

--- src/RocketSeller/TwoPickBundle/Controller/ProcedureController.php
+++ src/RocketSeller/TwoPickBundle/Controller/ProcedureController.php
@@ -16,11 +16,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ProcedureController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
         return $this->render(
-            'RocketSeller:TwoPickBundle:Default:index.html.twig',
-            array('name' => $name)
+            'RocketSellerTwoPickBundle:Registration:procedure.html.twig'
         );
     }
     /**
@@ -41,45 +40,61 @@ class ProcedureController extends Controller
 
     public function validateAction($id_employer, $id_procedure_type, $priority, $id_user, $employees)
     {		  		
-    		$em = $this->getDoctrine()->getManager();	
-    		$employerSearch = $this->loadClassById($id_employer,"Employer");
-    		$userSearch = $this->loadClassById($id_user,"User");
-    		$procedureTypeSearch = $this->loadClassById($id_procedure_type, "ProcedureType");
-			$procedure = new Procedure();
-			$procedure->setUserUser($userSearch);
-			$procedure->setProcedureTypeProcedureType($procedureTypeSearch);
-			$procedure->setEmployerEmployer($employerSearch);
-			$em->persist($procedure);
-			$em->flush();
-    											
-
+		$entityInscription = 1;
+		$em = $this->getDoctrine()->getManager();	
+		$employerSearch = $this->loadClassById($id_employer,"Employer");
+		$userSearch = $this->loadClassById($id_user,"User");
+		$procedureTypeSearch = $this->loadClassById($id_procedure_type, "ProcedureType");
+		$procedure = new RealProcedure();
+		$procedure->setUserUser($userSearch);
+		$procedure->setProcedureTypeProcedureType($procedureTypeSearch);
+		$procedure->setEmployerEmployer($employerSearch);
+		$em->persist($procedure);
+			    											
     		foreach ($employees as $employee) {
     			foreach ($employee["entities"] as $entity) {
     				$actionTypeFound = $this->loadClassById($entity["id_action_type"],"ActionType");
 		    		$employeeFound = $this->loadClassById($employee["id_employee"],"Employee");
     				$employeeHasEntityFound = $this->loadClassByArray(array("employeeEmployee" => $employeeFound),"EmployeeHasEntity");
 				    	if($employeeHasEntityFound && $employeeHasEntityFound->getEntityEntity()->getEntityTypeEntityType()->getIdEntityType() == $entity["id_entity"]){
-				    		echo "el empleado con id:". $employee["id_employee"]." esta afiliado a la entidad: ".$employeeHasEntityFound->getEntityEntity()->getEntityTypeEntityType()->getName().  "<br></br>";
 				    	}else{
-		    				$action = new Action();
+				    		if($this->loadClassByArray(array(
+				    			"personPerson" => $employeeFound->getPersonPerson(),
+				    			"actionTypeActionType" => $this->loadClassById(
+				            	$entityInscription,"ActionType")
+				    		),"Action")){
+				    			//se verifica que no hallan actions repetidos de inscripcion
+				    		}else{
+			    				$action = new Action();
+					            $action->setUserUser($userSearch);
+					            $action->setActionTypeActionType($this->loadClassById(
+					            	$entityInscription,"ActionType"));
+					            $action->setPersonPerson($employeeFound->getPersonPerson());
+					            $em->persist($action);
+					            $em->flush();
+				             	$procedure->addAction($action);	
+				    		}				    		
+				    	}
+				    	//se verifica que no hallan actions iguales.
+				    	if($this->loadClassByArray(array(
+				    			"personPerson" => $employeeFound->getPersonPerson(),
+				    			"actionTypeActionType" => $actionTypeFound
+				    		),"Action")){
+
+				    	}else{
+				    		$action = new Action();
 				            $action->setUserUser($userSearch);
 				            $action->setActionTypeActionType($actionTypeFound);
 				            $action->setPersonPerson($employeeFound->getPersonPerson());
 				            $em->persist($action);
 				            $em->flush();
-				            echo "el empleado con el id: " .$employee["id_employee"]. " no esta afiliado a la entidad con id: ". $entity["id_entity"]. "<br></br>";
 				            $procedure->addAction($action);
-				    	}
-	    				$action = new Action();
-			            $action->setUserUser($userSearch);
-			            $action->setActionTypeActionType($actionTypeFound);
-			            $action->setPersonPerson($employeeFound->getPersonPerson());
-			            $em->persist($action);
-			            $em->flush();
-
-			            $procedure->addAction($action);				 
+				    	}	    								
     			}
-    		}
+    		}    	
+    		$em->flush();
+        return $procedure;
+        
     }
     /**
      * hace un query de la clase para instanciarla
@@ -116,7 +131,6 @@ class ProcedureController extends Controller
     		$priority = 1;
     		$id_user = 1;
     		$id_contrato = 1; //preguntar para que el contrato?
-    		$id_action_type = 1; //inscripción
     		$employees = array(
     			array(
     				'id_employee' => 1,
@@ -128,7 +142,7 @@ class ProcedureController extends Controller
 	    			"entities" => array(
 			    				array(
 				    					'id_entity' => 1,
-				    					'id_action_type' => 2,
+				    					'id_action_type' => 3,
 				    					),
 			    				array(
 				    					'id_entity' => 2,
@@ -146,7 +160,7 @@ class ProcedureController extends Controller
 	    			"entities" => array(
 				    				array(
 					    					'id_entity' => 1,
-					    					'id_action_type' => 2,
+					    					'id_action_type' => 3,
 					    					),
 				    				array(
 					    					'id_entity' => 2,
@@ -155,7 +169,12 @@ class ProcedureController extends Controller
 			    				)
 		    				)
     			);
-    		$this->validateAction($id_employer, $id_procedure_type, $priority, $id_user, $employees);
+    		$procedures = $this->validateAction($id_employer, $id_procedure_type, $priority, $id_user, $employees);
+	        return $this->render(
+            'RocketSellerTwoPickBundle:Registration:procedure.html.twig',
+            array(
+            		'procedures' => $procedures
+            	));
 
     }
 
