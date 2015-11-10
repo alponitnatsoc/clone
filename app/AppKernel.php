47dbc013b8f912bf308b25e17ea3de580d6d85bf
gabrielsamoma <gabriel.montero@symplifica.com>
2015-11-10 11:56:37 -0500

    Primera Funcion Rest

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\AopBundle\JMSAopBundle(),
+            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             
             new RocketSeller\TwoPickBundle\RocketSellerTwoPickBundle(),
             //FOSUser
