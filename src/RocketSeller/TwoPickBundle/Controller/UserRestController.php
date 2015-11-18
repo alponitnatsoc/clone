fa22e458fd1b719191220aee14b42ca2b0579a18
Plinio Edgardo Romero Muñoz <romero.p.mfc@gmail.com>
2015-11-18 09:00:06 -0500

    Implementacion todo el servicio para determinar el estado de la suscripcion del usuario con base en la última orden de compra relacionada con el servicio de symplifica y en estado pago, teniendo en cuenta que su fecha de creación no sea superior a 30 días.

--- src/RocketSeller/TwoPickBundle/Controller/UserRestController.php
+++ src/RocketSeller/TwoPickBundle/Controller/UserRestController.php
@@ -9,6 +9,12 @@ use FOS\RestBundle\Request\ParamFetcher;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Component\Validator\ConstraintViolationList;
 use Symfony\Component\Validator\ConstraintViolation;
+use RocketSeller\TwoPickBundle\Entity\PurchaseOrders;
+use Symfony\Component\Validator\Constraints\Date;
+use RocketSeller\TwoPickBundle\Entity\User;
+use FOS\RestBundle\EventListener\ParamFetcherListener;
+use FOS\RestBundle\Request\ParamReader;
+use FOS\RestBundle\Tests\Request\ParamFetcherTest;
 
 class UserRestController extends FOSRestController
 {
@@ -307,14 +313,140 @@ class UserRestController extends FOSRestController
         $em = $this->getDoctrine()->getManager();
         $purchaseOrdersRepository = $em->getRepository("RocketSellerTwoPickBundle:PurchaseOrders");
 
+        /** @var PurchaseOrders $data */
         $data = $purchaseOrdersRepository->findOneBy(
-            array("idUser" => $id, "purchaseOrdersTypePurchaseOrdersType" => 2),
-            array("date_modified" => "DESC")
+            array(
+                "idUser" => $id,
+                "purchaseOrdersTypePurchaseOrdersType" => 2, //Tipo de orden de compra Servicio de Symplifica
+                "purchaseOrdersStatusPurchaseOrdersStatus" => 1 //Estatus de orden de compra Pagada
+            ),
+            array("date_created" => "DESC")
+        );
+
+        if ($data) {
+            $dateMod = $data->getDateCreated();
+            $date = new \DateTime();
+
+            $inicio = date( "Y-m-d ", $dateMod->getTimestamp());
+            $fin = date( "Y-m-d ", $date->getTimestamp());
+
+            $start_ts = strtotime($inicio);
+            $end_ts = strtotime($fin);
+            $diff = $end_ts - $start_ts;
+            $dateDiff = round(round($diff / 3600)/24);
+        }
+
+        if (isset($dateDiff) && $dateDiff <= 30) {
+            $response = array(
+                "code" => "1",
+                "response" => "Suscripcion Activa"
+            );
+        } else if (isset($dateDiff) && $dateDiff > 30) {
+            $response = array(
+                "code" => "0",
+                "response" => "Suscripcion Inactiva"
+            );
+        } else {
+            $response = array(
+                "code" => "2",
+                "response" => "El usuario no tiene suscripcion"
+            );
+        }
+
+        $view = View::create();
+        $view->setData($response)->setStatusCode(200);
+        return $view;
+    }
+
+    /**
+     * Actualizar estado de la suscripcion de un usuario
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   description = "Obtener todos los datos de una orden de compra.",
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     400 = "Returned when the form has errors"
+     *   }
+     * )
+     *
+     * @param ParamFetcher $paramFetcher
+     * @RequestParam(name="id", nullable=false, strict=true, description="id del usuario")
+     *
+     * @return \FOS\RestBundle\View\View
+     */
+    public function getUserStatusAction(ParamFetcher $paramFetcher)
+    {
+//         $id = $paramFetcher->get("id");
+$id = 1;
+        $status = $this->getUserActiveSuscriptionAction($id);
+
+        $data = $status->getData();
+        $code = $data["code"];
+
+        $userManager = $this->container->get("fos_user.user_manager");
+
+        /** @var User $user */
+        $user = $userManager->findUserBy(array('id'=>$id));
+        $statusActual = $user->getStatus();
+
+        if ($code != $statusActual) {
+            switch ($code) {
+                case 0: //Suscripcion Activa
+                    $this->updateUserStatus($id, 1);
+                    $resCode = 2;
+                    $msgCode = "La suscripción del usuario se ha activado";
+                    break;
+                case 1: //Sucripicion Inactiva
+                case 2: //No tiene suscripcion
+                default:
+                    $this->updateUserStatus($id, 0);
+                    $resCode = 1;
+                    $msgCode = "La suscripción del usuario se ha desactivado";
+                    break;
+            }
+        } else {
+            $resCode = 0;
+            $msgCode = "La suscripción del usuario no ha cambiado de estado";
+        }
+
+        $response = array(
+            "code" => $resCode,
+            "response" => $msgCode
         );
 
         $view = View::create();
-        $view->setData($data)->setStatusCode(200);
+        $view->setData($response)->setStatusCode(200);
         return $view;
+    }
+
+    protected function updateUserStatus($id, $status)
+    {
+        $userManager = $this->container->get("fos_user.user_manager");
+        $em = $this->getDoctrine()->getManager();
+
+        /** @var User $user */
+        $user = $userManager->findUserBy(array('id'=>$id));
+        $em->persist($user);
+        $em->flush();
+
+        if(isset($status)){
+            $user->setStatus($status);
+        }
+
+        $view = View::create();
+
+        $errors = $this->get('validator')->validate($user, array('Update'));
+
+        if (count($errors) == 0) {
+            $em->persist($user);
+            $em->flush();
+            $view->setData($user)->setStatusCode(200);
+            return $view;
+        } else {
+            $view = $this->getErrorsView($errors);
+            return $view;
+        }
     }
 
     /**
