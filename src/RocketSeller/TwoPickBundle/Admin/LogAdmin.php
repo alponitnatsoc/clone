2633939b0380621d5312e4de38803ff2f5ab4d43
nicomalacho <nicolasgaviria94@gmail.com>
2015-11-30 17:46:26 -0500

    Se crea el administrador de logs que solo se pueden visualizar, no editar o borrar

---
+++ src/RocketSeller/TwoPickBundle/Admin/LogAdmin.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace RocketSeller\TwoPickBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+class LogAdmin extends Admin
+{
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+    // to remove a single route
+    $collection->remove('delete');
+    // OR remove all route except named ones
+    $collection->clearExcept(array('list', 'show'));
+    }
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('type')
+            ->add('personPerson.names')
+            ->add('personPerson.document')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('type')
+            ->add('personPerson')
+            ->add('data')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array()
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('type')
+            ->add('personPerson')
+            ->add('data')
+        ;
+    }
+}
