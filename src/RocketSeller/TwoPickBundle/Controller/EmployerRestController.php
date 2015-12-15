9a59ecb3ecf2e04d520e8c60944e2ed6a5cb0403
Plinio Edgardo Romero Muñoz <romero.p.mfc@gmail.com>
2015-12-15 18:02:33 -0500

    Pasar metodo con la logica para determinar el detalle de una transaccion a trait que se pueda reutilizar en diferentes metodos para evitar la reescritura de codigo

--- src/RocketSeller/TwoPickBundle/Controller/EmployerRestController.php
+++ src/RocketSeller/TwoPickBundle/Controller/EmployerRestController.php
@@ -14,8 +14,11 @@ use RocketSeller\TwoPickBundle\Entity\User;
 use RocketSeller\TwoPickBundle\Entity\EmployerHasEmployee;
 use RocketSeller\TwoPickBundle\Entity\Liquidation;
 
+use RocketSeller\TwoPickBundle\Traits\GetTransactionDetailTrait;
+
 class EmployerRestController extends FOSRestController
 {
+    use GetTransactionDetailTrait;
     /**
      * Obtener el detalle de una transaccion
      *
@@ -36,59 +39,7 @@ class EmployerRestController extends FOSRestController
      */
     public function getTransactionDetailAction($type, $id)
     {
-        $em = $this->getDoctrine()->getManager();
-
-        $details = array();
-
-        switch ($type) {
-            case "contract":
-
-                $contractRepository = $em->getRepository("RocketSellerTwoPickBundle:Contract");
-                /** @var Contract $contract */
-                $contract = $contractRepository->findOneBy(
-                    array(
-                        "idContract" => $id
-                    )
-                );
-                $details["benefics"] = $contract->getBenefits();
-                $details["contractType"] = $contract->getContractTypeContractType();
-                $details["document"] = $contract->getDocumentDocument();
-                $details["employeeContractType"] = $contract->getEmployeeContractTypeEmployeeContractType();
-                $details["payMethod"] = $contract->getPayMethodPayMethod();
-                $details["payrolls"] = $contract->getPayrolls();
-                $details["position"] = $contract->getPositionPosition();
-                $details["salary"] = $contract->getSalary();
-                $details["state"] = $contract->getState();
-                $details["timeCommitment"] = $contract->getTimeCommitmentTimeCommitment();
-                $details["workplaces"] = $contract->getWorkplaces();
-                break;
-            case "pay":
-                $payRepository = $em->getRepository("RocketSellerTwoPickBundle:Pay");
-                /** @var Pay $pay */
-                $pay = $payRepository->findOneBy(
-                    array(
-                        "idPay" => $id
-                    )
-                );
-                $details["purchaseOrder"] = $pay->getPurchaseOrdersPurchaseOrders();
-                $details["payType"] = $pay->getPayTypePayType();
-                $details["payMethod"] = $pay->getPayMethodPayMethod();
-                break;
-            case "liquidation":
-                $liquidationRepository = $em->getRepository("RocketSellerTwoPickBundle:Liquidation");
-                /** @var Liquidation $liquidation */
-                $liquidation = $liquidationRepository->findOneBy(
-                    array(
-                        "id" => $id
-                    )
-                );
-                $details = $liquidation;
-                break;
-            case "novelty":
-                break;
-            default:
-                break;
-        }
+        $details = $this->transactionDetail($type, $id);
 
         $view = View::create();
         $view->setData($details)->setStatusCode(200);
---
+++ src/RocketSeller/TwoPickBundle/Traits/GetTransactionDetailTrait.php
@@ -0,0 +1,64 @@
+<?php
+namespace RocketSeller\TwoPickBundle\Traits;
+
+trait GetTransactionDetailTrait
+{
+    protected function transactionDetail($type, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $details = array();
+
+        switch ($type) {
+            case "contract":
+
+                $contractRepository = $em->getRepository("RocketSellerTwoPickBundle:Contract");
+                /** @var Contract $contract */
+                $contract = $contractRepository->findOneBy(
+                    array(
+                        "idContract" => $id
+                    )
+                    );
+                $details["benefics"] = $contract->getBenefits();
+                $details["contractType"] = $contract->getContractTypeContractType();
+                $details["document"] = $contract->getDocumentDocument();
+                $details["employeeContractType"] = $contract->getEmployeeContractTypeEmployeeContractType();
+                $details["payMethod"] = $contract->getPayMethodPayMethod();
+                $details["payrolls"] = $contract->getPayrolls();
+                $details["position"] = $contract->getPositionPosition();
+                $details["salary"] = $contract->getSalary();
+                $details["state"] = $contract->getState();
+                $details["timeCommitment"] = $contract->getTimeCommitmentTimeCommitment();
+                $details["workplaces"] = $contract->getWorkplaces();
+                break;
+            case "pay":
+                $payRepository = $em->getRepository("RocketSellerTwoPickBundle:Pay");
+                /** @var Pay $pay */
+                $pay = $payRepository->findOneBy(
+                    array(
+                        "idPay" => $id
+                    )
+                    );
+                $details["purchaseOrder"] = $pay->getPurchaseOrdersPurchaseOrders();
+                $details["payType"] = $pay->getPayTypePayType();
+                $details["payMethod"] = $pay->getPayMethodPayMethod();
+                break;
+            case "liquidation":
+                $liquidationRepository = $em->getRepository("RocketSellerTwoPickBundle:Liquidation");
+                /** @var Liquidation $liquidation */
+                $liquidation = $liquidationRepository->findOneBy(
+                    array(
+                        "id" => $id
+                    )
+                    );
+                $details = $liquidation;
+                break;
+            case "novelty":
+                break;
+            default:
+                break;
+        }
+
+        return $details;
+    }
+}
