b3d06a00af031dd9f8f1880ce35d8575c564d0be
gabrielsamoma <gabriel.montero@symplifica.com>
2016-01-19 17:18:18 -0500

    Telefonos Fix Ups

--- src/RocketSeller/TwoPickBundle/Controller/EmployeeRestController.php
+++ src/RocketSeller/TwoPickBundle/Controller/EmployeeRestController.php
@@ -1020,7 +1020,7 @@ class EmployeeRestController extends FOSRestController
      * @RequestParam(name="idEmployer", nullable=false, strict=true, description="employee type.")
      * @RequestParam(name="severances", nullable=false, strict=true, description="employee type.")
      * @RequestParam(name="arl", nullable=false, strict=true, description="employee type.")
-     * @RequestParam(name="economicalActivity", nullable=false, strict=true, description="employee type.")
+     * @RequestParam(name="economicalActivity", nullable=true, strict=true, description="employee type.")
      * @RequestParam(array=true, name="register_social_security", nullable=true, strict=true, description="afiliaciones")
      * @return View
      */
