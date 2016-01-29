7b28dae9e3eb85b5cd9c3746cc36b6e3d040fc29
Plinio Edgardo Romero Muñoz <romero.p.mfc@gmail.com>
2016-01-29 11:15:27 -0500

    Archivos liquidation

---
+++ src/RocketSeller/TwoPickBundle/Entity/LiquidationReason.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace RocketSeller\TwoPickBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * LiquidationReason
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class LiquidationReason
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255, nullable=TRUE)
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return LiquidationReason
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return LiquidationReason
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
