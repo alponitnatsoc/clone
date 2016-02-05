afe2d8dd2e25dfa8ef298861c6db5db0ba86aa3b
Diego Cano <dcanob@gmail.com>
2016-02-05 05:16:32 -0500

    Pago Nomina

--- src/RocketSeller/TwoPickBundle/Controller/PayrollController.php
+++ src/RocketSeller/TwoPickBundle/Controller/PayrollController.php
@@ -3,6 +3,7 @@
 namespace RocketSeller\TwoPickBundle\Controller;
 
 use RocketSeller\TwoPickBundle\Entity\Payroll;
+use RocketSeller\TwoPickBundle\Entity\PurchaseOrders;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -52,9 +53,17 @@ class PayrollController extends Controller
                                 $novelties[$employerHasEmployee->getIdEmployerHasEmployee()][] = $query->getQuery()->getResult()[0];
                             }
                         }
-
+                        $frecuecia = $contract->getPayMethodPayMethod()->getFrequencyFrequency()->getPayrollCode();
+                        if ($frecuecia == 'J') {
+                            $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary() / 30;
+                        } elseif ($frecuecia == 'M') {
+                            $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary() / 1;
+                        } elseif ($frecuecia == 'Q') {
+                            $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary() / 2;
+                        } else {
+                            $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary();
+                        }
                         $payrolls[$employerHasEmployee->getIdEmployerHasEmployee()] = $payroll;
-                        $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary();
                         $aportes[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary() * 0.04;
                     }
                 }
@@ -80,6 +89,7 @@ class PayrollController extends Controller
         }
         if ($request->isMethod('POST')) {
             $employees = $request->request->get('employee');
+            $payrolls = $request->request->get('payroll');
             $user = $this->getUser();
             $employer = $user->getPersonPerson()->getEmployer();
             $repository = $this->getDoctrine()
@@ -98,12 +108,21 @@ class PayrollController extends Controller
                     $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = 0;
                     foreach ($contracts as $contract) {
                         if ($contract->getState() == 'Active') {
-                            $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary();
                             $aportes[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary() * 0.04;
                             $payMethod[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getPayMethodPayMethod();
+                            $frecuecia = $contract->getPayMethodPayMethod()->getFrequencyFrequency()->getPayrollCode();
+                            if ($frecuecia == 'J') {
+                                $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary() / 30;
+                            } elseif ($frecuecia == 'M') {
+                                $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary() / 1;
+                            } elseif ($frecuecia == 'Q') {
+                                $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary() / 2;
+                            } else {
+                                $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary();
+                            }
                         }
+                        $total += $salaries[$employerHasEmployee->getIdEmployerHasEmployee()];
                     }
-                    $total += $salaries[$employerHasEmployee->getIdEmployerHasEmployee()];
                 }
             }
             return $this->render('RocketSellerTwoPickBundle:Payroll:calculate.html.twig', array(
@@ -111,6 +130,7 @@ class PayrollController extends Controller
                         "salaries" => $salaries,
                         "aportes" => $aportes,
                         "payMethod" => $payMethod,
+                        "payrolls" => $payrolls,
                         "total" => $total
             ));
         } else {
@@ -125,6 +145,7 @@ class PayrollController extends Controller
         }
         if ($request->isMethod('POST')) {
             $employees = $request->request->get('employee');
+            $payrolls = $request->request->get('payroll');
             $user = $this->getUser();
             $employer = $user->getPersonPerson()->getEmployer();
             $repository = $this->getDoctrine()
@@ -135,25 +156,82 @@ class PayrollController extends Controller
                     ->andWhere('e.employerEmployer = :employer')
                     ->setParameter('employer', $employer->getIdEmployer());
             $employeesData = $query->getQuery()->getResult();
-            $salaries = array();
+            $salaries = $aportes = $payMethod = array();
             $total = 0;
             foreach ($employeesData as $employerHasEmployee) {
-
-                $contracts = $employerHasEmployee->getContracts();
-                $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = 0;
-                foreach ($contracts as $contract) {
-                    $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary();
+                if ($employerHasEmployee->getState() == 'Active') {
+                    $contracts = $employerHasEmployee->getContracts();
+                    $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = 0;
+                    foreach ($contracts as $contract) {
+                        if ($contract->getState() == 'Active') {
+                            $aportes[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary() * 0.04;
+                            $payMethod[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getPayMethodPayMethod();
+                            $frecuecia = $contract->getPayMethodPayMethod()->getFrequencyFrequency()->getPayrollCode();
+                            if ($frecuecia == 'J') {
+                                $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary() / 30;
+                            } elseif ($frecuecia == 'M') {
+                                $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary() / 1;
+                            } elseif ($frecuecia == 'Q') {
+                                $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary() / 2;
+                            } else {
+                                $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] = $contract->getSalary();
+                            }
+                            $subTotal = $salaries[$employerHasEmployee->getIdEmployerHasEmployee()] + $aportes[$employerHasEmployee->getIdEmployerHasEmployee()];
+                            $total += $subTotal;
+                            $this->createPurchaseOrder($employerHasEmployee, $contract, $subTotal);
+                        }
+                    }
                 }
-                $total += $salaries[$employerHasEmployee->getIdEmployerHasEmployee()];
             }
             return $this->render('RocketSellerTwoPickBundle:Payroll:confirm.html.twig', array(
                         "employees" => $employeesData,
                         "salaries" => $salaries,
+                        "aportes" => $aportes,
+                        "payMethod" => $payMethod,
+                        "payrolls" => $payrolls,
                         "total" => $total
             ));
         } else {
             return $this->redirectToRoute("payroll");
         }
+    }
+
+    public function createPurchaseOrder($employerHasEmployee, $contract, $subTotal)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $purchaseOrder = new PurchaseOrders();
+
+        $purchaseOrdersType = $this->getDoctrine()
+                ->getRepository('RocketSellerTwoPickBundle:PurchaseOrdersType');
+        $purchaseOrdersType = $purchaseOrdersType->findBy(array('name' => 'Pago nomina'))[0];
+        $purchaseOrder->setPurchaseOrdersTypePurchaseOrdersType($purchaseOrdersType);
+
+        $payroll = $this->getDoctrine()
+                ->getRepository('RocketSellerTwoPickBundle:Payroll');
+        $payroll = $payroll->findBy(array('contractContract' => $contract->getIdContract()))[0];
+        $purchaseOrder->setPayrollPayroll($payroll);
+
+        $purchaseOrdersStatus = $this->getDoctrine()
+                ->getRepository('RocketSellerTwoPickBundle:PurchaseOrdersStatus');
+        $purchaseOrdersStatus = $purchaseOrdersStatus->findBy(array('name' => 'Pagada'))[0];
+        $purchaseOrder->setPurchaseOrdersStatusPurchaseOrdersStatus($purchaseOrdersStatus);
+
+
+        $dateCreated = date("Y-m-d H:i:s");
+        $purchaseOrder->setDateCreated(new \DateTime($dateCreated));
+        $dateModified = date("Y-m-d H:i:s");
+        $purchaseOrder->setDateModified(new \DateTime($dateModified));
+        $idUser = $this->getUser();
+        $purchaseOrder->setIdUser($idUser);
+        $purchaseOrder->setName('Pago Nomina');
+        $purchaseOrder->setValue($subTotal);
+
+        $em->persist($purchaseOrder);
+        $em->flush();
+
+        return true;
     }
 
     public function detailAction($idPayroll, Request $request)
@@ -163,6 +241,15 @@ class PayrollController extends Controller
         }
 
         return $this->render('RocketSellerTwoPickBundle:Payroll:detail.html.twig', array());
+    }
+
+    public function voucherAction($idPayroll, Request $request)
+    {
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw $this->createAccessDeniedException();
+        }
+
+        return $this->render('RocketSellerTwoPickBundle:Payroll:comprobante.html.twig', array());
     }
 
 }
