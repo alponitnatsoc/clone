ea04de3514d274c00f36f8d4470092063d900ac6
Diego Cano <dcanob@gmail.com>
2016-02-23 16:46:11 -0500

    Ajustes pago membresia paso 1

--- src/RocketSeller/TwoPickBundle/Controller/DashBoardController.php
+++ src/RocketSeller/TwoPickBundle/Controller/DashBoardController.php
@@ -21,7 +21,9 @@ class DashBoardController extends Controller
     {
         //¿Cómo vamos a hacer para saber en que parte del form está el usuario?
         //para el render se envía un array steps en el cuals e le puede agregar el estado el usuario
+        /* @var $user User */
         $user = $this->getUser();
+        $paymentState = $user->getPaymentState();
         $stateRegister = 0;
         $stateEmployees = 0;
         $stateAfiliation = 0;
@@ -42,19 +44,19 @@ class DashBoardController extends Controller
             //minima de un 100%
             //si existen empleados se puede empezar a subir el 0%
             if ($numEmployees > 0) {
-                $minUnit = 100 / ($numEmployees +1);
+                $minUnit = 100 / ($numEmployees + 1);
                 /** @var EmployerHasEmployee $value */
                 foreach ($employees as $key => $value) {
                     //para cada empleado se mira si tiene por lo menos 1 contrato
                     $stateEmployees+=$value->getEmployeeEmployee()->getRegisterState();
-                    if($value->getEmployeeEmployee()->getEntities()->count()!=0){
+                    if ($value->getEmployeeEmployee()->getEntities()->count() != 0) {
                         $stateAfiliation+=$minUnit;
                     }
                 }
-                if($employer->getEntities()->count()>0){
+                if ($employer->getEntities()->count() > 0) {
                     $stateAfiliation+=$minUnit;
                 }
-                $stateEmployees=$stateEmployees/$numEmployees;
+                $stateEmployees = $stateEmployees / $numEmployees;
             }
         }
 
@@ -89,7 +91,7 @@ class DashBoardController extends Controller
             $steps ['2'] = $step3;
         } else {
             $step2 = array(
-                'url' => $stateRegister!=100 ?"":$this->generateUrl('register_employee', array('id' => $idCurrentEmployee)),
+                'url' => $stateRegister != 100 ? "" : $this->generateUrl('register_employee', array('id' => $idCurrentEmployee)),
                 'name' => "Datos de los empleados",
                 'state' => $stateEmployees,
                 'paso' => 2,
@@ -98,13 +100,22 @@ class DashBoardController extends Controller
             $steps ['1'] = $step2;
         }
         $step4 = array(
-            'url' => $stateEmployees!=100 ?"":$this->generateUrl('matrix_choose'),
-            'name' => "Finalizar afiliación",
+            'url' => $paymentState ? "" : $this->generateUrl('subscription_choices'),
+            'name' => "Pago afiliación",
             'paso' => 3,
+            'state' => $paymentState,
+            'boxStyle' => "big",
+            'stateMessage' => !$paymentState ? "Iniciar" : "Editar",);
+        $steps ['3'] = $step4;
+
+        $step5 = array(
+            'url' => $stateEmployees != 100 ? "" : $this->generateUrl('matrix_choose'),
+            'name' => "Finalizar afiliación",
+            'paso' => 4,
             'state' => $stateAfiliation,
             'boxStyle' => "big",
             'stateMessage' => $stateAfiliation != 100 ? "Iniciar" : "Editar",);
-        $steps ['3'] = $step4;
+        $steps ['4'] = $step5;
 
         return $this->render('RocketSellerTwoPickBundle:General:dashBoard.html.twig', array('steps' => $steps));
     }
---
+++ web/public/js/subscriptionChoise.js
@@ -0,0 +1,183 @@
+function startSubscriptionChoise() {
+    var tiempo_parcialSlider, medio_tiempoSlider, tiempo_completoSlider;
+    $(document).ready(function () {
+
+        tiempo_parcialSlider = document.getElementById('tiempo_parcial');
+        noUiSlider.create(tiempo_parcialSlider, {
+            start: 0,
+            step: 1,
+            range: {
+                min: 0,
+                max: 5
+            },
+            pips: {
+                mode: 'values',
+                values: [0, 1, 2, 3, 4, 5],
+                density: 100
+            }
+        });
+        tiempo_parcialSlider.noUiSlider.on('set', function () {
+            calculatePrice("_calc");
+        });
+        tiempo_parcialSlider.noUiSlider.on('change', function () {
+            calculatePrice("_calc");
+        });
+        tiempo_parcialSlider.noUiSlider.set($(".activo > .trabajo_por_días").length);
+
+        medio_tiempoSlider = document.getElementById('medio_tiempo');
+        noUiSlider.create(medio_tiempoSlider, {
+            start: 0,
+            step: 1,
+            range: {
+                min: 0,
+                max: 5
+            },
+            pips: {
+                mode: 'values',
+                values: [0, 1, 2, 3, 4, 5],
+                density: 100
+            }
+        });
+        medio_tiempoSlider.noUiSlider.on('set', function () {
+            calculatePrice("_calc");
+        });
+        medio_tiempoSlider.noUiSlider.on('change', function () {
+            calculatePrice("_calc");
+        });
+        medio_tiempoSlider.noUiSlider.set($(".activo > .medio_tiempo").length);
+
+        tiempo_completoSlider = document.getElementById('tiempo_completo');
+        noUiSlider.create(tiempo_completoSlider, {
+            start: 0,
+            step: 1,
+            range: {
+                min: 0,
+                max: 5
+            },
+            pips: {
+                mode: 'values',
+                values: [0, 1, 2, 3, 4, 5],
+                density: 100
+            }
+        });
+        tiempo_completoSlider.noUiSlider.on('set', function () {
+            calculatePrice("_calc");
+        });
+        tiempo_completoSlider.noUiSlider.on('change', function () {
+            calculatePrice("_calc");
+        });
+        tiempo_completoSlider.noUiSlider.set($(".activo > .tiempo_completo").length);
+
+        calculatePrice('');
+    });
+
+    var contratoid = '';
+    var button = '';
+    $('#modal_confirm').on('show.bs.modal', function (event) {
+        button = $(event.relatedTarget);
+        contratoid = button.data('contrato-id');
+        if ($(".activo").length > 1 || $(button).html() == "Activar") {
+            $(".btn-change-state-contract-confirm").show();
+        } else {
+            $(".btn-change-state-contract-confirm").hide();
+        }
+    });
+    //$(".btn-change-state-contract").on('click', function (e) {
+    //    $('#modal_confirm').modal('show');
+    //});
+    $(".btn-change-state-contract-confirm").on('click', function (e) {
+        if ($(".activo").length > 1 || $(button).html() == "Activar") {
+            ajax(button, contratoid);
+        } else {
+            $('#modal_confirm').modal('hide');
+        }
+    });
+    //$("#open_pricing_calc").on('click', function (e) {
+    //    $('#modal_price_calculator').modal('show');
+    //});
+    function ajax(obj, url) {
+        $.ajax({
+            url: url,
+            type: 'GET'
+        }).done(function (data) {
+            parent = $(obj).parent().parent();
+            female = parent.find(".female").length;
+            male = parent.find(".male").length;
+            state = '';
+            if (data.state == 'Inactivo') {
+                $(obj).html("Activar");
+                if (female > 0) {
+                    state = "inactivada";
+                } else if (male > 0) {
+                    state = "inactivado";
+                }
+                parent.removeClass("activo");
+                parent.addClass("inactivo");
+            }
+            if (data.state == 'Activo') {
+                $(obj).html("Inactivar");
+                if (female > 0) {
+                    state = "activada";
+                } else if (male > 0) {
+                    state = "activado";
+                }
+                parent.removeClass("inactivo");
+                parent.addClass("activo");
+            }
+            $('#modal_confirm').modal('hide');
+            name = parent.find(".employee_name").html();
+            $('.result_ajax').html(name + " fue " + state + " exitosamente.").show(1000);
+            setTimeout(function () {
+                $('.result_ajax').html("").hide(1000);
+            }, 2000);
+            calculatePrice('');
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+            $('#modal_confirm').modal('hide');
+        });
+    }
+    function calculatePrice(contenedor) {
+        var Tiempo_Completo = 0, Medio_tiempo = 0, Trabajo_por_días = 0;
+        var descuento_html = '';
+        var descuento = 0;
+        var total = 0;
+        if (contenedor == '_calc') {
+            Tiempo_Completo = tiempo_completoSlider ? parseInt(tiempo_completoSlider.noUiSlider.get()) : 0;
+            Medio_tiempo = medio_tiempoSlider ? parseInt(medio_tiempoSlider.noUiSlider.get()) : 0;
+            Trabajo_por_días = tiempo_parcialSlider ? parseInt(tiempo_parcialSlider.noUiSlider.get()) : 0;
+        } else {
+            Tiempo_Completo = $(".activo > .tiempo_completo").length;
+            Medio_tiempo = $(".activo > .Medio_tiempo").length;
+            Trabajo_por_días = $(".activo > .trabajo_por_días").length;
+        }
+        if (Tiempo_Completo > 0) {
+            total = total + (Tiempo_Completo * 25000);
+        }
+        if (Medio_tiempo > 0) {
+            total = total + (Medio_tiempo * 20000);
+        }
+        if (Trabajo_por_días > 0) {
+            total = total + (Trabajo_por_días * 15000);
+        }
+
+        if ((Tiempo_Completo + Medio_tiempo + Trabajo_por_días) >= 3) {
+            descuento = (total * 0.1);
+            descuento_html = "El descuento del 10% por valor de " + getPrice(descuento) + " ya fue aplicado.";
+            $("#result_discount" + contenedor).html(descuento_html);
+        } else {
+            $("#result_discount" + contenedor).html('');
+        }
+
+        $("#result_price" + contenedor).html(getPrice(total - descuento));
+    }
+    function getPrice(valor) {
+        price = parseFloat(valor.toString().replace(/,/g, ""))
+                .toFixed(0)
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        return "$ " + price;
+    }
+}
+
