645e2bc163a8c9adac2f2c4ae0ab45a6e0f90952
gabrielsamoma <gabriel.montero@symplifica.com>
2016-03-02 12:24:42 -0500

    added method-id to the response

--- src/RocketSeller/TwoPickBundle/Controller/PaymentMethodRestController.php
+++ src/RocketSeller/TwoPickBundle/Controller/PaymentMethodRestController.php
@@ -102,9 +102,9 @@ class PaymentMethodRestController extends FOSRestController
         ));
         $insertionAnswer = $this->forward('RocketSellerTwoPickBundle:PaymentsRest:deleteReversePaymentMethod', array('_format' => 'json'));
         if($insertionAnswer->getStatusCode()!=200){
-            $view->setStatusCode(200)->setData(array('error'=>array("Credit Card"=>"Se agrego la taerjeta de Credito, pero no se pudo reversar el cobro")));
+            $view->setStatusCode(200)->setData(array('response'=>array("method-id"=>$idPayM),'error'=>array("Credit Card"=>"Se agrego la taerjeta de Credito, pero no se pudo reversar el cobro")));
         }else{
-            $view->setStatusCode($insertionAnswer->getStatusCode())->setData($insertionAnswer->getContent());
+            $view->setStatusCode($insertionAnswer->getStatusCode())->setData(array('response'=>array("method-id"=>$idPayM),"extra-data"=>$insertionAnswer->getContent()));
         }
         return $view;
 
