4e0165b3e6a5e753f3ddfbfd9ba6ff922118e30c
Juan Luis Gómez C <juanluis.gomez@blipblip.com.co>
2016-03-07 15:09:25 -0500

    Slide Login

--- web/js/public/main.js
+++ web/js/public/main.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
 	$('#btnLogin').click(function(evt) {
 		evt.preventDefault();
+		evt.stopPropagation();
 		console.log("Prueba");
 		$('#slideLogin').toggleClass('active');
 		$('#btnLogin .fa-inverse').toggleClass('fa-caret-up');
 		$('#btnLogin .fa-inverse').toggleClass('fa-caret-down');
 	});
+
+	$('html').click(function() {
+		$('#slideLogin').removeClass('active');
+		$('#btnLogin .fa-inverse').addClass('fa-caret-up');
+		$('#btnLogin .fa-inverse').removeClass('fa-caret-down');
+	});
+
 });
