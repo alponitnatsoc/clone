2f6324d7c7f889a1694ab26c8332a0ad229f57a4
Plinio Edgardo Romero Muñoz <romero.p.mfc@gmail.com>
2016-03-13 22:30:14 -0500

    Textos campos requeridos

--- web/public/js/liquidation.js
+++ web/public/js/liquidation.js
@@ -9,6 +9,26 @@ function validateLiqForm() {
                 "rocketseller_twopickbundle_liquidation[liquidationReason]": "Por favor selecciona una opción"
             }
         });
+
+        jQuery.extend(jQuery.validator.messages, {
+            required: "Este campo es requerido",
+            remote: "Please fix this field.",
+            email: "Ingresa un email válido",
+            url: "Please enter a valid URL.",
+            date: "Inrega una fecha válida",
+            dateISO: "Please enter a valid date (ISO).",
+            number: "Please enter a valid number.",
+            digits: "Please enter only digits.",
+            creditcard: "Ingresa un número de tarjeta de crédito válido",
+            equalTo: "Please enter the same value again.",
+            accept: "Please enter a value with a valid extension.",
+            maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+            minlength: jQuery.validator.format("Please enter at least {0} characters."),
+            rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
+            range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+            max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
+            min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
+        });
     });
 
     $("#liquidation-newstep1").click(function(e) {
