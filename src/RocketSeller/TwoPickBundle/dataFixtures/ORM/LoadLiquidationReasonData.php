bc4ccc91ad70d9e775d18e358a4013d47817d195
Plinio Edgardo Romero Muñoz <romero.p.mfc@gmail.com>
2016-03-14 10:22:26 -0500

    Quitar razones de liquidacion que no se van a utilizar Cambiar copy despido con y sin justa causa

--- src/RocketSeller/TwoPickBundle/dataFixtures/ORM/LoadLiquidationReasonData.php
+++ src/RocketSeller/TwoPickBundle/dataFixtures/ORM/LoadLiquidationReasonData.php
@@ -12,41 +12,41 @@ class LiquidationReasonData extends AbstractFixture implements OrderedFixtureInt
     public function load(ObjectManager $manager)
     {
 
-        $cambioRegimen = new LiquidationReason();
-        $cambioRegimen->setName('Cambio de regimen laboral a integral');
-        $cambioRegimen->setDescription('Cambio de regimen laboral a integral');
-        $cambioRegimen->setPayrollCode('1');
-        $manager->persist($cambioRegimen);
+//         $cambioRegimen = new LiquidationReason();
+//         $cambioRegimen->setName('Cambio de regimen laboral a integral');
+//         $cambioRegimen->setDescription('Cambio de regimen laboral a integral');
+//         $cambioRegimen->setPayrollCode('1');
+//         $manager->persist($cambioRegimen);
 
         $liquidationReasonJustaCausa = new LiquidationReason();
-        $liquidationReasonJustaCausa->setName('Despido con justa causa');
+        $liquidationReasonJustaCausa->setName('Terminación del contrato con justa causa');
         $liquidationReasonJustaCausa->setDescription('El empleado incumplió el reglamento interno de trabajo');
         $liquidationReasonJustaCausa->setPayrollCode('2');
         $manager->persist($liquidationReasonJustaCausa);
 
         $liquidationReasonSinJustaCausa = new LiquidationReason();
-        $liquidationReasonSinJustaCausa->setName('Despido sin justa causa');
+        $liquidationReasonSinJustaCausa->setName('Terminación del contrato sin justa causa');
         $liquidationReasonSinJustaCausa->setDescription('El empleador finaliza el contrato sin una causa justa');
         $liquidationReasonSinJustaCausa->setPayrollCode('3');
         $manager->persist($liquidationReasonSinJustaCausa);
 
-        $eliminacionCargo = new LiquidationReason();
-        $eliminacionCargo->setName('Eliminación cargo');
-        $eliminacionCargo->setDescription('Se elimina el cargo que ocupa el empleado');
-        $eliminacionCargo->setPayrollCode('4');
-        $manager->persist($eliminacionCargo);
+//         $eliminacionCargo = new LiquidationReason();
+//         $eliminacionCargo->setName('Eliminación cargo');
+//         $eliminacionCargo->setDescription('Se elimina el cargo que ocupa el empleado');
+//         $eliminacionCargo->setPayrollCode('4');
+//         $manager->persist($eliminacionCargo);
 
-        $fallecimiento = new LiquidationReason();
-        $fallecimiento->setName('Fallecimiento');
-        $fallecimiento->setDescription('Fallecimiento');
-        $fallecimiento->setPayrollCode('5');
-        $manager->persist($fallecimiento);
+//         $fallecimiento = new LiquidationReason();
+//         $fallecimiento->setName('Fallecimiento');
+//         $fallecimiento->setDescription('Fallecimiento');
+//         $fallecimiento->setPayrollCode('5');
+//         $manager->persist($fallecimiento);
 
-        $pension = new LiquidationReason();
-        $pension->setName('Pension');
-        $pension->setDescription('Pension');
-        $pension->setPayrollCode('6');
-        $manager->persist($pension);
+//         $pension = new LiquidationReason();
+//         $pension->setName('Pension');
+//         $pension->setDescription('Pension');
+//         $pension->setPayrollCode('6');
+//         $manager->persist($pension);
 
         $LiquidationReasonRenuncia = new LiquidationReason();
         $LiquidationReasonRenuncia->setName('Renuncia');
@@ -54,17 +54,17 @@ class LiquidationReasonData extends AbstractFixture implements OrderedFixtureInt
         $LiquidationReasonRenuncia->setPayrollCode('7');
         $manager->persist($LiquidationReasonRenuncia);
 
-        $vencimientoContrato = new LiquidationReason();
-        $vencimientoContrato->setName('Vencimiento de contrato');
-        $vencimientoContrato->setDescription('Vencimiento de contrato');
-        $vencimientoContrato->setPayrollCode('8');
-        $manager->persist($vencimientoContrato);
+//         $vencimientoContrato = new LiquidationReason();
+//         $vencimientoContrato->setName('Vencimiento de contrato');
+//         $vencimientoContrato->setDescription('Vencimiento de contrato');
+//         $vencimientoContrato->setPayrollCode('8');
+//         $manager->persist($vencimientoContrato);
 
-        $periodoPrueba = new LiquidationReason();
-        $periodoPrueba->setName('Periodo de prueba');
-        $periodoPrueba->setDescription('Periodo de prueba');
-        $periodoPrueba->setPayrollCode('9');
-        $manager->persist($periodoPrueba);
+//         $periodoPrueba = new LiquidationReason();
+//         $periodoPrueba->setName('Periodo de prueba');
+//         $periodoPrueba->setDescription('Periodo de prueba');
+//         $periodoPrueba->setPayrollCode('9');
+//         $manager->persist($periodoPrueba);
 
         $LiquidationReasonAcuerdo = new LiquidationReason();
         $LiquidationReasonAcuerdo->setName('Mutuo acuerdo');
@@ -74,15 +74,15 @@ class LiquidationReasonData extends AbstractFixture implements OrderedFixtureInt
 
         $manager->flush();
 
-        $this->addReference('liquidationReason-cambio-regimen', $cambioRegimen);
+//         $this->addReference('liquidationReason-cambio-regimen', $cambioRegimen);
         $this->addReference('liquidationReason-justa-causa', $liquidationReasonJustaCausa);
         $this->addReference('liquidationReason-sin-justa-causa', $liquidationReasonSinJustaCausa);
-        $this->addReference('liquidationReason-eliminacion-contrato', $eliminacionCargo);
-        $this->addReference('liquidationReason-fallecimiento', $fallecimiento);
-        $this->addReference('liquidationReason-pension', $pension);
+//         $this->addReference('liquidationReason-eliminacion-contrato', $eliminacionCargo);
+//         $this->addReference('liquidationReason-fallecimiento', $fallecimiento);
+//         $this->addReference('liquidationReason-pension', $pension);
         $this->addReference('liquidationReason-renuncia', $LiquidationReasonRenuncia);
-        $this->addReference('liquidationReason-vencimineto-contrato', $vencimientoContrato);
-        $this->addReference('liquidationReason-periodo-prueba', $periodoPrueba);
+//         $this->addReference('liquidationReason-vencimineto-contrato', $vencimientoContrato);
+//         $this->addReference('liquidationReason-periodo-prueba', $periodoPrueba);
         $this->addReference('liquidationReason-acuerdo', $LiquidationReasonAcuerdo);
 
     }
