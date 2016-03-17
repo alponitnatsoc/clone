bf96efa5c1e30cd445c1be7b1f72940c773713ba
Diego Cano <dcanob@gmail.com>
2016-03-17 17:18:17 -0500

    Registro express un solo form

--- src/RocketSeller/TwoPickBundle/Form/RegistrationExpress.php
+++ src/RocketSeller/TwoPickBundle/Form/RegistrationExpress.php
@@ -1,5 +1,6 @@
 <?php
- namespace RocketSeller\TwoPickBundle\Form;
+
+namespace RocketSeller\TwoPickBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -9,9 +10,12 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use RocketSeller\TwoPickBundle\Form\AddCreditCard;
+use RocketSeller\TwoPickBundle\Form\PhoneRegistration;
 
 class RegistrationExpress extends AbstractType
 {
+
     /**
      * La funcion que agrega los campos extra al formulario del registro que se renderiza completo
      * @param el formulario y las opciones que recibe la funcion para crear el formulario
@@ -19,69 +23,75 @@ class RegistrationExpress extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('documentType', 'choice', array(
-                'choices' => array(
-                    'CC'   => 'Cédula de ciudadanía',
-                    'CE' => 'Cedula de extranjería',
-                    'TI' => 'Tarjeta de identidad'
-                ),
-                'multiple' => false,
-                'expanded' => false,
-                'property_path' => 'documentType',
-                'label' => 'Tipo de documento*',
-                'placeholder' => 'Tu tipo de documento',
-                'required' => true
-            ))
-            ->add('document', 'text', array(
-                'constraints' => array(
-                    new NotBlank()
-                ),
-                'property_path' => 'document',
-                'label' => 'Número de documento*',
-                "attr" => array(
-                    "data-toggle" => "tooltip", 
-                    "data-placement" => "right",
-                    "data-container" => "body",
-                    "title" => "Con esto validaremos tu información en las entidades de salud",
-                    'placeholder' => 'Número de documento',
-                )
-            ))
-            ->add('names', 'text', 
-                array(
+                ->setAction($options['action'])
+                ->setMethod($options['method'])
+                ->add('documentType', 'choice', array(
+                    'choices' => array(
+                        'CC' => 'Cédula de ciudadanía',
+                        'CE' => 'Cedula de extranjería',
+                        'TI' => 'Tarjeta de identidad'
+                    ),
+                    'multiple' => false,
+                    'expanded' => false,
+                    'property_path' => 'documentType',
+                    'label' => 'Tipo de documento*',
+                    'placeholder' => 'Tu tipo de documento',
+                    'required' => true
+                ))
+                ->add('document', 'integer', array(
+                    'constraints' => array(
+                        new NotBlank()
+                    ),
+                    'property_path' => 'document',
+                    'label' => 'Número de documento*',
+                    "attr" => array(
+                        "data-toggle" => "tooltip",
+                        "data-placement" => "right",
+                        "data-container" => "body",
+                        "title" => "Con esto validaremos tu información en las entidades de salud",
+                        'placeholder' => 'Número de documento',
+                    )
+                ))
+                ->add('names', 'text', array(
                     'constraints' => array(
                         new NotBlank()
                     ),
                     "attr" => array('placeholder' => 'Tus Nombres'),
                     'label' => 'Nombres*'
+                        )
                 )
-            )
-            ->add('lastName1', 'text', array(
-                'constraints' => array(
-                    new NotBlank()
-                ),
-                "attr" => array('placeholder' => 'Primer apellido'),
-                'label' => 'Primer Apellido*'))
-            ->add('lastName2', 'text', array(
-                'constraints' => array(
-                    new NotBlank()
-                ),
-                'label' => 'Segundo Apellido',
-                "attr" => array('placeholder' => 'Segundo apellido'),
-                'required' => false
-            ))
-            ->add('save', 'submit', array(
-                'label' => 'Continuar',
-                'attr'  => array(
-                                'class' => 'btn register-AS1 btn-symplifica'
-                            ),
-                
-            ));
-            
+                ->add('lastName1', 'text', array(
+                    'constraints' => array(
+                        new NotBlank()
+                    ),
+                    "attr" => array('placeholder' => 'Primer apellido'),
+                    'label' => 'Primer Apellido*'))
+                ->add('lastName2', 'text', array(
+                    'constraints' => array(
+                        new NotBlank()
+                    ),
+                    'label' => 'Segundo Apellido',
+                    "attr" => array('placeholder' => 'Segundo apellido'),
+                    'required' => false
+                ))
+                ->add('phone', new PhoneRegistration(), array(
+                    'mapped' => false
+                ))
+                ->add('credit_card', new AddCreditCard(), array(
+                    'mapped' => false
+                ))
+                ->add('save', 'submit', array(
+                    'label' => 'Continuar',
+                    'attr' => array(
+                        'class' => 'btn register-AS1 btn-symplifica'
+                    ),
+        ));
     }
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'RocketSeller\TwoPickBundle\Entity\Person',            
+            'data_class' => 'RocketSeller\TwoPickBundle\Entity\Person',
         ));
     }
 
@@ -89,4 +99,5 @@ class RegistrationExpress extends AbstractType
     {
         return 'app_user_express_registration';
     }
+
 }
