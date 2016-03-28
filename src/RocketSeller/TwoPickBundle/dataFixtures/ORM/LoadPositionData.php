af0524d11fd8ee0b7c9f36616710d260a8f25aee
Plinio Edgardo Romero Muñoz <romero.p.mfc@gmail.com>
2016-03-28 11:22:49 -0500

    Solo los cargos que vamos a manejar Empleada domestica, conductor, niñera, mayordomo y enfermera

--- src/RocketSeller/TwoPickBundle/dataFixtures/ORM/LoadPositionData.php
+++ src/RocketSeller/TwoPickBundle/dataFixtures/ORM/LoadPositionData.php
@@ -16,28 +16,28 @@ class LoadPositionData extends AbstractFixture implements OrderedFixtureInterfac
         $PositionDomestica->setPayrollCoverageCode('1'); // Code 0.522.
         $manager->persist($PositionDomestica);
 
-        $PositionJardinero = new Position();
-        $PositionJardinero->setName('Jardinero(a)');
-        $PositionJardinero->setPayrollCoverageCode('3'); //  Code 2.436
-        $manager->persist($PositionJardinero);
+//         $PositionJardinero = new Position();
+//         $PositionJardinero->setName('Jardinero(a)');
+//         $PositionJardinero->setPayrollCoverageCode('3'); //  Code 2.436
+//         $manager->persist($PositionJardinero);
 
-        $PositionVarios = new Position();
-        $PositionVarios->setName('Oficios Varios');
-        $PositionVarios->setPayrollCoverageCode('1'); // Code 0.522
-        $manager->persist($PositionVarios);
+//         $PositionVarios = new Position();
+//         $PositionVarios->setName('Oficios Varios');
+//         $PositionVarios->setPayrollCoverageCode('1'); // Code 0.522
+//         $manager->persist($PositionVarios);
 
         $PositionOtros = new Position();
-        $PositionOtros->setName('Conductor');
+        $PositionOtros->setName('Conductor(a)');
         $PositionOtros->setPayrollCoverageCode('4'); // Code 4.35
         $manager->persist($PositionOtros);
 
         $ninera = new Position();
-        $ninera->setName('Niñera');
+        $ninera->setName('Niñero(a)');
         $ninera->setPayrollCoverageCode('1'); // Code 0.522
         $manager->persist($ninera);
 
         $enfermera = new Position();
-        $enfermera->setName('Enfermera');
+        $enfermera->setName('Enfermero(a)');
         $enfermera->setPayrollCoverageCode('1'); // Code 0.522
         $manager->persist($enfermera);
 
@@ -46,21 +46,21 @@ class LoadPositionData extends AbstractFixture implements OrderedFixtureInterfac
         $mayordomo->setPayrollCoverageCode('3'); // Code 2.436
         $manager->persist($mayordomo);
 
-        $amaLlaves = new Position();
-        $amaLlaves->setName('Ama de Llaves ');
-        $amaLlaves->setPayrollCoverageCode('1'); // Code 0.522
-        $manager->persist($amaLlaves);
+//         $amaLlaves = new Position();
+//         $amaLlaves->setName('Ama de Llaves ');
+//         $amaLlaves->setPayrollCoverageCode('1'); // Code 0.522
+//         $manager->persist($amaLlaves);
 
         $manager->flush();
 
         $this->addReference('position-domestico', $PositionDomestica);
-        $this->addReference('position-jardinero', $PositionJardinero);
-        $this->addReference('position-varios', $PositionVarios);
+//         $this->addReference('position-jardinero', $PositionJardinero);
+//         $this->addReference('position-varios', $PositionVarios);
         $this->addReference('position-otros', $PositionOtros);
         $this->addReference('position-ninera', $ninera);
         $this->addReference('position-enfermera', $enfermera);
         $this->addReference('position-mayordomo', $mayordomo);
-        $this->addReference('position-amaLlaves', $amaLlaves);
+//         $this->addReference('position-amaLlaves', $amaLlaves);
     }
     public function getOrder()
     {
