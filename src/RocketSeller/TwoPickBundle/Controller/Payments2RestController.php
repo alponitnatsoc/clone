cb61b8a693d94a5473220400e5cc8ce00ff05183
Daniel Serrano <dserrano3@gmail.com>
2016-04-08 14:36:48 -0500

    Adding insert and consult client plus small fixes

--- src/RocketSeller/TwoPickBundle/Controller/Payments2RestController.php
+++ src/RocketSeller/TwoPickBundle/Controller/Payments2RestController.php
@@ -23,9 +23,72 @@ use EightPoints\Bundle\GuzzleBundle;
 class DocumentoSoporte {
         var $nombre;
         var $base64;
-        function MemberLoginCredentials($a,$b) {
+        function DocumentoSoporte($a,$b) {
           $this->nombre = $a;
           $this->base64 = $b;
+        }
+}
+
+class InfoBasica {
+        var $primer_apellido;
+        var $segundo_apellido;
+        var $primer_nombre;
+        var $segundo_nombre;
+        var $tipo_documento;
+        var $numero_documento;
+        var $fecha_expedicion;
+        var $residente_colombiano;
+        var $estado_civil;
+        var $lugar_nacimiento;
+        var $direccion_residencia;
+        var $telefono_residencia;
+        var $municipio_residencia;
+        var $departamento_residencia;
+        var $direccion_oficina;
+        var $telefono_oficina;
+        var $fax_oficina;
+        var $municipio_oficina;
+        var $departamento_oficina;
+        var $celular;
+        var $correo;
+        var $administra_recursos_publicos;
+
+        function __construct($a,$b,$c,$d,$e,$f,$g,$h,$i,$j,$k,$l,$m,
+                                        $n,$o,$p,$q,$r,$s,$t,$u,$v) {
+          $this->primer_apellido = $a;
+          $this->segundo_apellido = $b;
+          $this->primer_nombre = $c;
+          $this->segundo_nombre = $d;
+          $this->tipo_documento = $e;
+          $this->numero_documento= $f;
+          $this->fecha_expedicion = $g;
+          $this->residente_colombiano = $h;
+          $this->estado_civil = $i;
+          $this->lugar_nacimiento = $j;
+          $this->direccion_residencia = $k;
+          $this->telefono_residencia = $l;
+          $this->municipio_residencia = $m;
+          $this->departamento_residencia = $n;
+          $this->direccion_oficina = $o;
+          $this->telefono_oficina = $p;
+          $this->fax_oficina = $q;
+          $this->municipio_oficina = $r;
+          $this->departamento_oficina = $s;
+          $this->celular = $t;
+          $this->correo = $u;
+          $this->administra_recursos_publicos = $v;
+        }
+}
+
+class InfoEstudio {
+        var $nivel_estudios;
+        var $titulo_obtenido;
+        var $ocupacion;
+
+        function InfoEstudio($a=null,$b=null,$c=null) {
+          $this->nivel_estudios = $a;
+          $this->titulo_obtenido = $b;
+          $this->ocupacion = $c;
         }
 }
 
@@ -130,6 +193,154 @@ class Payments2RestController extends FOSRestController
 
 
     /**
+     * Registers a natural person as an employer.<br/>
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   description = " Registers a natural person as an employer.",
+     *   statusCodes = {
+     *     200 = "Created",
+     *     400 = "Bad Request",
+     *     404 = "Not found",
+     *     422 = "Bad parameters"
+     *   }
+     * )
+     *
+     * @param Request $request.
+     * Rest Parameters:
+     *
+     * (name="firstLastName", nullable=false, requirements="(.)*", strict=true, description="Code of the bank, can be found int he table Bank.")
+     * (name="secondLastName", nullable=true, requirements="(.)*", strict=true, description="Checking or saving account.")
+     * (name="firstFirstName", nullable=false, requirements="(.)*", strict=true, description="Number of the bank account.")
+     * (name="secondFirstName", nullable=true, requirements="(.)*", strict=true, description="Name of the file of the letter authorizing symplifica.")
+     * (name="documentType", nullable=false, requirements="(CC|cc|nit|NIT)", strict=true, description="File of the letter authorizing symplifica, in base 64.")
+     * (name="documentNumber", nullable=false, requirements="\d+", strict=true, description="File of the letter authorizing symplifica, in base 64.")
+     * (name="documentExpeditionDate", nullable=false, requirements="[0-9]{4}-[0-9]{2}-[0-9]{2}", strict=true, description="YYYY-MM-DD")
+     * (name="civil_state", nullable=false, requirements="(SOLTERO|CASADO|UNION LIBRE|VIUDO|DIVORCIADO)", strict=true, description="")
+     * (name="address", nullable=false, requirements="(.)*", strict=true, description="")
+     * (name="phone", nullable=false, requirements="\d+", strict=true, description="")
+     * (name="municipio", nullable=false, requirements="(.)*", strict=true, description="")
+     * (name="department", nullable=false, requirements="(.)*", strict=true, description="")
+     * (name="mail", nullable=false, requirements="(.)*", strict=true, description="")
+     *
+     * @return View
+     */
+    public function postRegisterNaturalPersonAction(Request $request)
+    {
+        $path = "VinculacionPN";
+        $parameters = $request->request->all();
+        $regex = array();
+        $mandatory = array();
+        // Set all the parameters info.
+        $regex['firstLastName'] = '(.)*';
+        $mandatory['firstLastName'] = true;
+        $regex['secondLastName'] = '(.)*';
+        $mandatory['secondLastName'] = false;
+        $regex['firstFirstName'] = '(.)*';
+        $mandatory['firstFirstName'] = true;
+        $regex['secondFirstName'] = '(.)*';
+        $mandatory['secondFirstName'] = false;
+        $regex['documentType'] = '(CC|cc|nit|NIT)';
+        $mandatory['documentType'] = true;
+        $regex['documentNumber'] = '\d+';
+        $mandatory['documentNumber'] = true;
+        $regex['documentExpeditionDate'] = '[0-9]{4}-[0-9]{2}-[0-9]{2}';
+        $mandatory['documentExpeditionDate'] = true;
+        $regex['civil_state'] = '(SOLTERO|CASADO|UNION LIBRE|VIUDO|DIVORCIADO)';
+        $mandatory['civil_state'] = true;
+        $regex['address'] = '(.)*';
+        $mandatory['address'] = true;
+        $regex['phone'] = '\d+';
+        $mandatory['phone'] = true;
+        $regex['municipio'] = '(.)*';
+        $mandatory['municipio'] = true;
+        $regex['department'] = '(.)*';
+        $mandatory['department'] = true;
+        $regex['mail'] = '(.)*';
+        $mandatory['mail'] = true;
+
+        $this->validateParamters($parameters, $regex, $mandatory);
+
+        $parameters_fixed = array();
+        $info_estudio = new InfoEstudio();
+        $info_basica =
+        new InfoBasica($parameters['firstLastName'],
+                       $parameters['secondLastName'],
+                       $parameters['firstFirstName'],
+                       $parameters['secondFirstName'],
+                       $parameters['documentType'],
+                       $parameters['documentNumber'],
+                       $parameters['documentExpeditionDate'],
+                       null,
+                       $parameters['civil_state'],
+                       null,
+                       $parameters['address'],
+                       $parameters['phone'],
+                       $parameters['municipio'],
+                       $parameters['department'],
+                       null,
+                       null,
+                       null,
+                       null,
+                       null,
+                       null,
+                       $parameters['mail'],
+                       null,
+                       null,
+                       null,
+                       null,
+                       null,
+                       null,
+                       null,
+                       null,
+                       null,
+                       null,
+                       null,
+                       null,
+                       null,
+                       null
+                     );
+        $parameters_fixed['info_basica'] = $info_basica;
+        $parameters_fixed['info_estudio'] = $info_estudio;
+
+        /** @var View $res */
+        $responseView = $this->callApi($parameters_fixed, $path, "VinculacionPN");
+
+        return $responseView;
+    }
+
+    /**
+     * Gets the employer information.<br/>
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   description = "Gets the employer information.",
+     *   statusCodes = {
+     *     200 = "Created",
+     *     400 = "Bad Request",
+     *     404 = "Not found",
+     *     422 = "Bad parameters"
+     *   }
+     * )
+     *
+     * @param Int $radicatedNumber Radicated number, returned in created method.
+     *
+     * @return View
+     */
+    public function getEmployerAction($radicatedNumber)
+    {
+        $path = "ConsultarApertura";
+
+        $parameters_fixed = array();
+        $parameters_fixed['numeroRadicado'] = $radicatedNumber;
+
+        /** @var View $res */
+        $responseView = $this->callApi($parameters_fixed, $path, "ConsultarApertura");
+
+        return $responseView;
+    }
+
+    /**
      * Registers a new bank account for an employer.<br/>
      *
      * @ApiDoc(
@@ -151,6 +362,7 @@ class Payments2RestController extends FOSRestController
      * (name="bankCode", nullable=false, requirements="(([0-9|-]| )+|(GS|PL))", strict=true, description="Code of the bank, can be found int he table Bank.")
      * (name="accountType", nullable=false, requirements="(AH|CC)", strict=true, description="Checking or saving account.")
      * (name="bankAccountNumber", nullable=false, requirements="([0-9|-]| +)", strict=true, description="Number of the bank account.")
+     * (name="expirationDate", nullable=false, requirements="[0-9]{4}-[0-9]{2}-[0-9]{2}", strict=true, description="Expiration date for this account(YYYY-MM-DD).")
      * (name="autorizationDocumentName", nullable=false, requirements="(.)*", strict=true, description="Name of the file of the letter authorizing symplifica.")
      * (name="autorizationDocument", nullable=false, requirements="", strict=true, description="File of the letter authorizing symplifica, in base 64.")
      *
@@ -171,6 +383,8 @@ class Payments2RestController extends FOSRestController
         $mandatory['accountType'] = true;
         $regex['bankAccountNumber'] = '([0-9|-]| )+';
         $mandatory['bankAccountNumber'] = true;
+        $regex['expirationDate'] = '[0-9]{4}-[0-9]{2}-[0-9]{2}';
+        $mandatory['expirationDate'] = true;
         $regex['autorizationDocumentName'] = '(.)*';
         $mandatory['autorizationDocumentName'] = true;
         //$regex['autorizationDocument'] = ''; No regex, binary.
@@ -183,6 +397,7 @@ class Payments2RestController extends FOSRestController
         $parameters_fixed['codBanco'] = $parameters['bankCode'];
         $parameters_fixed['tipoCuenta'] = $parameters['accountType'];
         $parameters_fixed['numeroCuenta'] = $parameters['bankAccountNumber'];
+        $parameters_fixed['fechaVencimiento'] = $parameters['expirationDate'];
         $parameters_fixed['documentoSoporteAutorizacion'] =
         new DocumentoSoporte($parameters['autorizationDocumentName'], $parameters['autorizationDocument']);
 
@@ -429,6 +644,7 @@ class Payments2RestController extends FOSRestController
      */
     public function getEmployerPaymentMethodsAction($accountNumber)
     {
+       // TODO(daniel.serrano): Arreglar el formato de respuesta de este servicio(IMPORTANTE!).
         $path = "ConsultarFuentesPago";
 
         $parameters_fixed = array();
@@ -545,8 +761,9 @@ class Payments2RestController extends FOSRestController
      * (name="documentEmployee", nullable=false, requirements="([0-9|-]| )+", strict=true, description="Document number of the employee.")
      * (name="bankCode", nullable=false, requirements="(([0-9|-]| )+|(GS|PL))", strict=true, description="Code of the bank of the employee. This can be obtained in the beneficiary.")
      * (name="accountType", nullable=false, requirements="(AH|CC|EN)", strict=true, description="Account type of the employee savings checking.")
-     * (name="accountNumber", nullable=false, requirements="([0-9|-]| )+", strict=true, description="Account number of the employee, real number not internal.")
+     * (name="accountBankNumber", nullable=false, requirements="([0-9|-]| )+", strict=true, description="Account number of the employee, real number not internal.")
      * (name="value", nullable=false, requirements="[0-9]+(\.[0-9]+)?", strict=true, description="value of the transaction.")
+     * (name="reference", nullable=true, requirements="(.)*", strict=false, description="Reference number for the pila, only if its different from the document number.")
      *
      * @return View
      */
@@ -567,10 +784,12 @@ class Payments2RestController extends FOSRestController
         $mandatory['bankCode'] = true;
         $regex['accountType'] = '(AH|CC|EN)';
         $mandatory['accountType'] = true;
-        $regex['accountNumber'] = '([0-9|-]| )+';
-        $mandatory['accountNumber'] = true;
+        $regex['accountBankNumber'] = '([0-9|-]| )+';
+        $mandatory['accountBankNumber'] = true;
         $regex['value'] = '[0-9]+(\.[0-9]+)?';
         $mandatory['value'] = true;
+        $regex['reference'] = '(.)*';
+        $mandatory['reference'] = false;
 
         $this->validateParamters($parameters, $regex, $mandatory);
 
@@ -589,8 +808,10 @@ class Payments2RestController extends FOSRestController
         $parameters_fixed['numeroDocumentoBeneficiario'] = $parameters['documentEmployee'];
         $parameters_fixed['codBanco'] = $parameters['bankCode'];
         $parameters_fixed['tipoCuenta'] = $parameters['accountType'];
-        $parameters_fixed['numeroCuenta'] = $parameters['accountNumber'];
+        $parameters_fixed['numeroCuenta'] = $parameters['accountBankNumber'];
         $parameters_fixed['valor'] = $parameters['value'];
+        if(isset($parameters['reference']))
+          $parameters_fixed['referencia'] = $parameters['reference'];
 
         /** @var View $res */
         $responseView = $this->callApi($parameters_fixed, $path, "RegistrarOrdenPago");
