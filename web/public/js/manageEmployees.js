619d00f362f044505571d28923d3a6c4608d4526
Nicolas Gaviria <nicolasgaviria94@gmail.com>
2016-04-11 17:30:33 -0500

    editar empleado en ficha de empleado

--- web/public/js/manageEmployees.js
+++ web/public/js/manageEmployees.js
@@ -1,6 +1,58 @@
 /**
  * Created by gabrielsamoma on 3/13/16.
  */
+function hideInfo(){
+    $(".bottom-box").fadeOut('slow');
+    
+}
+function showEmployeeForm(){
+    $(".bottom-box-edit").attr("hidden",false);
+    $(".bottom-box-edit").fadeOut('slow');
+    $(".bottom-box-edit").fadeIn('slow');
+
+}
+function editEmployee(){
+    $('.editEmployee').click(function(e){
+        hideInfo();
+        showEmployeeForm();
+        $(".editEmployee").hide();
+        e.preventDefault();
+        
+         $.ajax({
+                url: $(this).attr('href'),
+                type: 'GET',
+
+            }).done(function (data) {                            
+                var civilStatus = data['civilStatus'];
+                var year = data['birthDate']['year'];
+                var month = data['birthDate']['month'];
+                var day = data['birthDate']['day'];
+                var birthCountry = data['birthCountry'];
+                var birthDepartment = data['birthDepartment'];
+                var birthCity = data['birthCity'];
+                var mainAddress = data['mainAddress'];
+                var department = data['department'];
+                var city = data['city'];
+                var phone = data['phone'];
+                var email = data['email'];
+
+                $("select[name='form[civilStatus]']").val(civilStatus);
+                $("select[name='form[birthDate][year]']").val(year);
+                $("select[name='form[birthDate][month]']").val(month);
+                $("select[name='form[birthDate][day]']").val(day);
+                $("input[name='form[mainAddress]']").val(mainAddress);
+                $("input[name='form[phone]']").val(phone);
+                $("input[name='form[email]']").val(email);
+                $("select[name='form[department]']").val(department);
+                $("select[name='form[city]']").val(city);
+                $("select[name='form[birthDepartment]']").val(birthDepartment);
+                $("select[name='form[birthCity]']").val(birthCity);
+                $("select[name='form[birthCountry]']").val(birthCountry);
+            });     
+    });
+}
+
+
 function manageEmployees(){
     $(".addNovelty").each(function(){
         $(this).on("click", function(e){
