f03bce67a429a5a74587e716e645a915a436d1da
Diego Cano <dcanob@gmail.com>
2016-04-15 13:36:03 -0500

    FIX para cargar datos de bancos

--- src/RocketSeller/TwoPickBundle/dataFixtures/ORM/LoadBankData.php
+++ src/RocketSeller/TwoPickBundle/dataFixtures/ORM/LoadBankData.php
@@ -1,4 +1,5 @@
 <?php
+
 // src/AppBundle/DataFixtures/ORM/LoadUserData.php
 
 namespace RocketSeller\TwoPickBundle\DataFixtures\ORM;
@@ -15,155 +16,156 @@ use RocketSeller\TwoPickBundle\Entity\PayMethodFields;
 
 class LoadBankData extends AbstractFixture implements OrderedFixtureInterface
 {
+
     public function load(ObjectManager $manager)
     {
 
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO DE LA REPÚBLICA");
         $banco->setNovopaymentCode("00");
         $banco->setHightechCode("00");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO DE BOGOTÁ");
         $banco->setNovopaymentCode("01");
         $banco->setHightechCode("01");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO POPULAR");
         $banco->setNovopaymentCode("02");
         $banco->setHightechCode("02");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO CORPBANCA COLOMBIA S.A.");
         $banco->setNovopaymentCode("06");
         $banco->setHightechCode("06");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCOLOMBIA S.A.");
         $banco->setNovopaymentCode("07");
         $banco->setHightechCode("07");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("CITIBANK COLOMBIA");
         $banco->setNovopaymentCode("09");
         $banco->setHightechCode("09");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO GNB SUDAMERIS COLOMBIA");
         $banco->setNovopaymentCode("12");
         $banco->setHightechCode("12");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BBVA COLOMBIA");
         $banco->setNovopaymentCode("13");
         $banco->setHightechCode("13");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("HELM BANK");
         $banco->setNovopaymentCode("06");
         $banco->setHightechCode("14");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("RED MULTIBANCA COLPATRIA S.A.");
         $banco->setNovopaymentCode("42");
         $banco->setHightechCode("19");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO DE OCCIDENTE");
         $banco->setNovopaymentCode("23");
         $banco->setHightechCode("23");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCOLDEX");
         $banco->setNovopaymentCode("1");
         $banco->setHightechCode("31");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO CAJA SOCIAL - BCSC S.A.");
         $banco->setNovopaymentCode("30");
         $banco->setHightechCode("32");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO AGRARIO DE COLOMBIA S.A.");
         $banco->setNovopaymentCode("43");
         $banco->setHightechCode("40");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO DAVIVIENDA S.A.");
         $banco->setNovopaymentCode("39");
         $banco->setHightechCode("51");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO AV VILLAS");
         $banco->setNovopaymentCode("49");
         $banco->setHightechCode("52");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO WWB S.A.");
         $banco->setNovopaymentCode("53");
         $banco->setHightechCode("53");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO PROCREDIT");
         $banco->setNovopaymentCode("51");
-        $banco->setHightechCode(58"");
+        $banco->setHightechCode("58");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCAMIA");
         $banco->setNovopaymentCode("52");
         $banco->setHightechCode("59");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO PICHINCHA S.A.");
         $banco->setNovopaymentCode("57");
         $banco->setHightechCode("60");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCOOMEVA");
         $banco->setNovopaymentCode("54");
         $banco->setHightechCode("61");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO FALABELLA S.A.");
         $banco->setNovopaymentCode("56");
         $banco->setHightechCode("62");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO FINANDINA S.A.");
         $banco->setNovopaymentCode("55");
         $banco->setHightechCode("63");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO SANTANDER");
         $banco->setNovopaymentCode("59");
         $banco->setHightechCode("65");
         $manager->persist($banco);
 
-        $banco= new Bank();
+        $banco = new Bank();
         $banco->setName("BANCO COOPERATIVO COOPCENTRAL");
         $banco->setNovopaymentCode("58");
         $banco->setHightechCode("66");
@@ -171,10 +173,12 @@ class LoadBankData extends AbstractFixture implements OrderedFixtureInterface
 
         $manager->flush();
     }
+
     public function getOrder()
     {
         // the order in which fixtures will be loaded
         // the lower the number, the sooner that this fixture is loaded
         return 9;
     }
+
 }
