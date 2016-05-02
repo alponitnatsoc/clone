fd8827c99cdb1b0cf6d5a42153820b84f766d0a9
Diego Cano <Diego Cano>
2016-05-02 17:23:31 -0500

    rest para crear tramites

--- src/RocketSeller/TwoPickBundle/Controller/SubscriptionRestController.php
+++ src/RocketSeller/TwoPickBundle/Controller/SubscriptionRestController.php
@@ -10,13 +10,15 @@ use FOS\RestBundle\Controller\Annotations\RequestParam;
 use FOS\RestBundle\Request\ParamFetcher;
 use RocketSeller\TwoPickBundle\Traits\SubscriptionMethodsTrait;
 
-class SubscriptionRestController extends FOSRestController {
+class SubscriptionRestController extends FOSRestController
+{
 
     use SubscriptionMethodsTrait;
 
-    private function paySubscriptionToDays($daysToPay) {
+    private function paySubscriptionToDays($daysToPay)
+    {
         $users = $this->getDoctrine()->getRepository('RocketSeller\TwoPickBundle\Entity\User')->findBy(
-                array('dayToPay' => $daysToPay)
+            array('dayToPay' => $daysToPay)
         );
         $responses = array();
 
@@ -33,7 +35,7 @@ class SubscriptionRestController extends FOSRestController {
             }
             return $responses;
         } else {
-            
+
         }
         return false;
     }
@@ -43,7 +45,8 @@ class SubscriptionRestController extends FOSRestController {
      * @param User $user
      * @return boolean
      */
-    private function paySubscriptionUser(User $user) {
+    private function paySubscriptionUser(User $user)
+    {
         $day = $this->getDaysSince($user->getLastPayDate(), date_create(date('Y-m-d')));
         if ($day === true || ($day->d >= 28) || ($day->m >= 1)) {
             if ($this->createPurchaceOrder($user)) {
@@ -54,6 +57,39 @@ class SubscriptionRestController extends FOSRestController {
 //en caso que el pago no se pueda realizar que hay que hacer? 
 
         return false;
+    }
+
+    /**
+     * crear tramites para backOffice
+     *
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   description = "crear tramites para backOffice",
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     400 = "Returned when the mail no send"
+     *   }
+     * )
+     *
+     * @param ParamFetcher $paramFetcher
+     *
+     * @RequestParam(name="idUser", description="Recibe el id del usuario")
+     *
+     *
+     *
+     * @return View
+     */
+    public function postCrearTramitesAction(ParamFetcher $paramFetcher)
+    {
+        $idUser = ($paramFetcher->get('idUser'));
+        /* @var $user User */
+        $user = $this->getUserById($idUser);
+        $tramites = $this->crearTramites($user);
+        $view = View::create();
+        $view->setData($tramites);
+        $view->setStatusCode(200);
+        return $view;
     }
 
     /**
@@ -72,12 +108,13 @@ class SubscriptionRestController extends FOSRestController {
      * @param ParamFetcher $paramFetcher
      *
      * @RequestParam(name="idUser", description="Recibe el id del usuario")
-     * 
-     * 
+     *
+     *
      *
      * @return View
      */
-    public function postSubscriptionPayAction(ParamFetcher $paramFetcher) {
+    public function postSubscriptionPayAction(ParamFetcher $paramFetcher)
+    {
         $idUser = ($paramFetcher->get('idUser'));
         /* @var $user User */
         $user = $this->getUserById($idUser);
@@ -112,7 +149,8 @@ class SubscriptionRestController extends FOSRestController {
      *
      * @return View
      */
-    public function putSubscriptionPayAction() {
+    public function putSubscriptionPayAction()
+    {
         $daysToPay = array(28, 29, 30, 31);
         //$lasDayOfMonth = intval(date("t", strtotime("2015-03-23")));
         $lasDayOfMonth = intval(date("t"));
