c75f91a4a9797ac01b546365035e6777fd41068d
AndresFelipe <alponitnatsnoc@gmail.com>
2016-06-21 16:13:37 -0500

    pruebas dump

--- src/RocketSeller/TwoPickBundle/Entity/Person.php
+++ src/RocketSeller/TwoPickBundle/Entity/Person.php
@@ -616,11 +616,12 @@ class Person
         /** @var Document */
         $documents=$this->docs;
         foreach ($documents as $document){
-
+            dump('hola');
             if($document->getDocumentTypeDocumentType()==$docType){
                 return $document;
             }
         }
+        dump('fin');
         return 0;
     }
 
