a05d6f85031f5cda72e645b2b17e73131a6958e8
Daniel Rico <danieleo90@gmail.com>
2016-06-27 17:18:04 -0500

    Added support for custom help form

--- src/RocketSeller/TwoPickBundle/Form/Type/ContactType.php
+++ src/RocketSeller/TwoPickBundle/Form/Type/ContactType.php
@@ -15,11 +15,13 @@ class ContactType extends AbstractType
     private $name;
     private $email;
     private $phone;
+    private $subject;
 
-    public function __construct($name,$email,$phone){
+    public function __construct($name,$email,$phone,$subject = "default"){
         $this->name  = $name;
         $this->email = $email;
         $this->phone = $phone;
+        $this->subject = $subject;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
@@ -84,33 +86,38 @@ class ContactType extends AbstractType
                     )
                 );
         }
-        $builder
-            ->add('subject', 'choice', array(
-                    'label'   =>'Con que necesitas ayuda?',
-                    'choices' => array(
-                        0 => 'Registro',
-                        1 => 'Nómina y aportes',
-                        2 => 'Calculadora salarial',
-                        3 => 'Consulta jurídica',
-                        4 => 'Planes y precios',
-                        5 => 'Otros'
-                    ),
-                    'multiple' => false,
-                    'expanded' => false,
-                    'mapped'   => false,
-                    'required' => true
-                )
-            )
-        ->add('message', 'textarea', array(
-            'attr' => array(
-                'placeholder' => 'Escribe tu mensaje aquí, recibiras respuesta del equipo de symplifica lo antes posible'
-            )
-        ))
 
-        ->add('enviar', 'submit', array(
-            'label' => 'Enviar',
-            'attr'=> array('class'=>"naranja bold btn-symplifica btn", 'id'=>"submit_button", 'type'=>"submit")
-        ));
+        if($this->subject == "default"){
+          $builder
+              ->add('subject', 'choice', array(
+                      'label'   =>'Con que necesitas ayuda?',
+                      'choices' => array(
+                          0 => 'Registro',
+                          1 => 'Nómina y aportes',
+                          2 => 'Calculadora salarial',
+                          3 => 'Consulta jurídica',
+                          4 => 'Planes y precios',
+                          5 => 'Otros'
+                      ),
+                      'multiple' => false,
+                      'expanded' => false,
+                      'mapped'   => false,
+                      'required' => true
+                  )
+              );
+        }
+
+        $builder
+          ->add('message', 'textarea', array(
+              'attr' => array(
+                  'placeholder' => 'Escribe tu mensaje aquí, recibiras respuesta del equipo de symplifica lo antes posible'
+              )
+          ))
+
+          ->add('enviar', 'submit', array(
+              'label' => 'Enviar',
+              'attr'=> array('class'=>"naranja bold btn-symplifica btn", 'id'=>"submit_button", 'type'=>"submit")
+          ));
 
     }
 
