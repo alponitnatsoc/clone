abe3d613b17e35d590f08097417f0d8d29efa0a7
gabrielsamoma <gabriel.montero@symplifica.com>
2016-06-29 22:10:03 -0500

    NotificationApi

--- src/RocketSeller/TwoPickBundle/Controller/EmployerRestController.php
+++ src/RocketSeller/TwoPickBundle/Controller/EmployerRestController.php
@@ -2,11 +2,13 @@
 
 namespace RocketSeller\TwoPickBundle\Controller;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\RequestParam;
 use FOS\RestBundle\View\View;
 use FOS\RestBundle\Request\ParamFetcher;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use RocketSeller\TwoPickBundle\Entity\Notification;
 use RocketSeller\TwoPickBundle\Entity\Person;
 use RocketSeller\TwoPickBundle\Traits\EmployeeMethodsTrait;
 use Symfony\Component\Validator\ConstraintViolationList;
@@ -516,6 +518,44 @@ class EmployerRestController extends FOSRestController
             dump('nay');
             return $view->setStatusCode(500);
         }
+
+    }
+
+    /**
+     * Obtener las notificaciones activas del usuario
+     *
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   description = "Enviar email requerido",
+     *   statusCodes = {
+     *     200 = "Returned when successful",
+     *     404 = "Returned when no user found"
+     *   }
+     * )
+     *
+     * @param $userId
+     * @return View
+
+     */
+    public function getNotificationByUserAction($userId)
+    {
+
+        $view=View::create();
+        $userRepo=$this->getDoctrine()->getRepository("RocketSellerTwoPickBundle:User");
+        /** @var User $user */
+        $user=$userRepo->find($userId);
+        if($user==null){
+            return $view->setStatusCode(404);
+        }
+        $notifications=$user->getPersonPerson()->getNotifications();
+        $activeNotifications = new ArrayCollection();
+        /** @var Notification $notif */
+        foreach ( $notifications as $notif ) {
+            if( $notif->getStatus() == 1 )
+                $activeNotifications->add($notif);
+        }
+        return $view->setData(array('notifications'=>$activeNotifications))->setStatusCode(200);
 
     }
 
