f298b70e0245bc7cafc8ce9574e1a77b9719045e
Alfredo Santamaria <chino.sebastian@gmail.com>
2016-08-23 09:11:46 -0500

    adding api to change worked days

--- src/RocketSeller/TwoPickBundle/Controller/NoveltyRestController.php
+++ src/RocketSeller/TwoPickBundle/Controller/NoveltyRestController.php
@@ -17,6 +17,7 @@ use RocketSeller\TwoPickBundle\Entity\Person;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use RocketSeller\TwoPickBundle\Entity\WeekWorkableDays;
 use RocketSeller\TwoPickBundle\Entity\Workplace;
+use RocketSeller\TwoPickBundle\Controller\NoveltyController;
 use Symfony\Component\Validator\ConstraintViolationList;
 use DateTime;
 
@@ -50,6 +51,99 @@ class NoveltyRestController extends FOSRestController
 
      return $view->setStatusCode(200)->setData(array('novelties'=>$resultNoveltyTypes));
    }
+
+   /**
+    * update worked days<br/>
+    *
+    * @ApiDoc(
+    *   resource = true,
+    *   description = "update worked days",
+    *   statusCodes = {
+    *     200 = "Returned when successful"
+    *   }
+    * )
+    *
+    * @param paramFetcher $paramFetcher ParamFetcher
+    *
+    * @RequestParam(name="empId", nullable=false, strict=true, description="employee id")
+    * @RequestParam(name="idNovelty", nullable=false, strict=true, description="novelty id")
+    * @RequestParam(name="daysAmount", nullable=false, strict=true, description="daysAmount")
+    * @RequestParam(name="idPerson", nullable=false, strict=true, description="id person of logged user")
+    *
+    * @return View
+    */
+    public function postUpdateWorkedDaysAction(ParamFetcher $paramFetcher) {
+
+      $empId = $paramFetcher->get('empId');
+      $idNovelty = $paramFetcher->get('idNovelty');
+      $daysAmount = $paramFetcher->get('daysAmount');
+      $idPerson = $paramFetcher->get('idPerson');
+      $params = array(
+          'empId' => $empId,
+          'idNovelty' => $idNovelty,
+          'daysAmount' => $daysAmount,
+          'idPerson' => $idPerson
+      );
+      $result = $this->forward('RocketSellerTwoPickBundle:Novelty:updateWorkedDays', $params);
+      // updateWorkedDaysAction($empId, $idNovelty, $daysAmount);
+
+      $view = View::create();
+
+      return $view->setStatusCode(200)->setData(array('result'=>$result));
+    }
+
+   /**
+    * registers a new novelty<br/>
+    *
+    * @ApiDoc(
+    *   resource = true,
+    *   description = "registers a new novelty",
+    *   statusCodes = {
+    *     200 = "Returned when successful"
+    *   }
+    * )
+    *
+    * @param paramFetcher $paramFetcher ParamFetcher
+    *
+    * @RequestParam(name="noveltyFields", nullable=false, strict=true, description="novelty fields")
+    * @RequestParam(name="novletyTypeId", nullable=false, strict=true, description="novelty type")
+    * @RequestParam(name="payrrollId", nullable=false, strict=true, description="payrroll")
+    *
+    * @return View
+    */
+    public function postRegisterNoveltyAction(ParamFetcher $paramFetcher) {
+
+      $noveltyFields = $paramFetcher->get('noveltyFields');
+      $novletyTypeId = $paramFetcher->get('novletyTypeId');
+      $payrrollId = $paramFetcher->get('payrrollId');
+
+      $novletyType = $this->getDoctrine()
+          ->getRepository('RocketSellerTwoPickBundle:NoveltyType')
+          ->find($novletyTypeId);
+
+      $payroll = $this->getDoctrine()
+          ->getRepository('RocketSellerTwoPickBundle:Payroll')
+          ->find($payrrollId);
+
+      $novelty = new Novelty();
+
+      $novelty->setName($noveltyFields['name']);
+      if($noveltyFields['dateStart'])
+        $novelty->setDateStart($noveltyFields['dateStart']);
+      if($noveltyFields['dateStart'])
+        $novelty->setDateStart($noveltyFields['dateStart']);
+      if($noveltyFields['units'])
+        $novelty->setUnits($noveltyFields['units']);
+      if($noveltyFields['amount'])
+        $novelty->setAmount($noveltyFields['amount']);
+
+      //esperar a daniel
+      $result = "hola";
+      // $result = this->validateAndPersistNovelty($novelty, $payrroll, $noveltyType);
+      $view = View::create();
+
+      return $view->setStatusCode(200)->setData(array('result'=>$result));
+    }
 
     /**
      * Add the novelty<br/>
