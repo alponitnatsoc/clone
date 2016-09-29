296131ce8dc9c20515efb73278b12ade715775a8
Daniel Rico <danieleo90@gmail.com>
2016-09-29 11:07:59 -0500

    removing dump Kappa

--- src/RocketSeller/TwoPickBundle/Controller/BackOfficeController.php
+++ src/RocketSeller/TwoPickBundle/Controller/BackOfficeController.php
@@ -1404,7 +1404,6 @@ class BackOfficeController extends Controller
 		$podNomina = $this->getdoctrine()->getRepository('RocketSellerTwoPickBundle:PurchaseOrdersDescription')->findBy(array("productProduct"=>$product->getIdProduct()));
 		$podPila = $this->getdoctrine()->getRepository('RocketSellerTwoPickBundle:PurchaseOrdersDescription')->findBy(array("productProduct"=>$product2->getIdProduct()));
 		
-		dump($podPila);
 		//Now Pod has all the products nomina on the database.
 		return $this->render('RocketSellerTwoPickBundle:BackOffice:payState.html.twig', array('podsN' => $podNomina, 'podsP' => $podPila));
 	}
