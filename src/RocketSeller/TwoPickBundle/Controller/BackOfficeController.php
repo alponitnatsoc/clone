14b0af8eefeeb1c8d2ecc5a0ba2e806609985dea
gabrielmontero <gabriel.montero@symplifica.com>
2016-12-26 10:28:15 -0500

    Fixing pods

--- src/RocketSeller/TwoPickBundle/Controller/BackOfficeController.php
+++ src/RocketSeller/TwoPickBundle/Controller/BackOfficeController.php
@@ -2515,7 +2515,7 @@ class BackOfficeController extends Controller
 
 		$em=$this->getDoctrine()->getManager();
 
-		$payrolls = $this->getDoctrine()->getRepository("RocketSellerTwoPickBundle:Payroll")->findBy(array("period" => 4, "month" => 11, "year" => 2016, "paid" => 1));
+		$payrolls = $this->getDoctrine()->getRepository("RocketSellerTwoPickBundle:Payroll")->findBy(array("period" => 4, "month" => 12, "year" => 2016, "paid" => 1));
 		$pos = $this->getDoctrine()->getRepository("RocketSellerTwoPickBundle:PurchaseOrdersStatus")->findOneBy(array("idNovoPay" => "P1"));
 		$product = $this->getDoctrine()->getRepository("RocketSellerTwoPickBundle:Product")->findOneBy(array("simpleName" => "PP"));
 
