94881872d68306904e3f129f8ca9d9cbc25a0fce
a-santamaria <chino.sebastian@gmail.com>
2017-01-11 15:31:43 -0500

    adding service to retrieve referred code info

---
+++ src/RocketSeller/TwoPickBundle/Controller/ReferredRestSecuredController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace RocketSeller\TwoPickBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
+use RocketSeller\TwoPickBundle\Entity\Campaign;
+use RocketSeller\TwoPickBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use FOS\RestBundle\Controller\Annotations\RequestParam;
+use FOS\RestBundle\Request\ParamFetcher;
+use Symfony\Component\HttpFoundation\Response;
+
+class ReferredRestSecuredController extends FOSRestController
+{
+	
+	/**
+	 * get referred info (code, money, campaignStock) <br/>
+	 *
+	 * @ApiDoc(
+	 *   resource = true,
+	 *   description = "get download document",
+	 *   statusCodes = {
+	 *     200 = "OK",
+	 *     400 = "Bad Request",
+	 *     401 = "Unauthorized",
+	 *     404 = "Not Found"
+	 *   }
+	 * )
+	 *
+	 *
+	 * @return View
+	 */
+	public function getReferredPromoInfoAction() {
+		/** @var User $user */
+		$user = $this->getUser();
+		
+		/** @var Campaign $campaignRef */
+		$campaignRef = $this->getDoctrine()->getRepository('RocketSellerTwoPickBundle:Campaign')
+			->findOneBy(array('description' => 'RefCamp'));
+			
+		$view = View::create();
+		$view->setStatusCode(200);
+		$view->setData(array(
+			'campaignStock' => $campaignRef->getStock(),
+			'money' => $user->getMoney(),
+			'code' => $user->getCode(),
+			'textCampaign' => 'En Enero gana $' . number_format($campaignRef->getStock()) . ' por cada referido que complete su registro',
+			'textShareSocial' => 'Symplifica me ha ayudado a formalizar la relación laboral con mi empleada.\nUsa mi código y recibe 1 mes gratis para usar la plataforma, solo debes entrar al link \n'
+		));
+		
+		return $view;
+	}
+	
+}
