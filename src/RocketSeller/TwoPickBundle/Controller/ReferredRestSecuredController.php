00dc185c8ead6587a84ee7ad006ac7158a294c50
a-santamaria <chino.sebastian@gmail.com>
2017-01-31 12:20:23 -0500

    normalizing characters for sms

--- src/RocketSeller/TwoPickBundle/Controller/ReferredRestSecuredController.php
+++ src/RocketSeller/TwoPickBundle/Controller/ReferredRestSecuredController.php
@@ -78,8 +78,10 @@ class ReferredRestSecuredController extends FOSRestController
 		/** @var User $user */
 		$user = $this->getUser();
 		
-		$message = $user->getPersonPerson()->getFullName() .
-		            " te ha invitado a usar Symplifica para que formalices la relación laboral con tu empleada. Te contactaremos pronto. Recibirás 2 meses GRATIS";
+		$utils = $this->get('app.symplifica_utils');
+		
+		$message = $utils->normalizeAccentedChars($user->getPersonPerson()->getFullName()) .
+		            " te ha invitado a usar Symplifica para que formalices la relacion laboral con tu empleada. Te contactaremos pronto. Recibiras 2 meses GRATIS";
 		
 		$twilio = $this->get('twilio.api');
 		$twilio->account->messages->sendMessage("+19562671001", $phone, $message);
